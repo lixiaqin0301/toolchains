8dbc22ded9a20d69e030f1005f04063b7d9e1461
Li Xiaqin <lixiaqin0301@gmail.com>
2021-11-25 13:51:20 +0800

    Build curl git

--- buildsh/build_curl.sh
+++ buildsh/build_curl.sh
@@ -3,76 +3,77 @@
 export CC=/home/lixq/toolchains/gcc/bin/gcc
 export CXX=/home/lixq/toolchains/gcc/bin/g++
 export CPP=/home/lixq/toolchains/gcc/bin/cpp
-export LIBRARY_PATH=/home/lixq/toolchains/gcc/lib64:/home/lixq/toolchains/curl-7.79.1/thirdparty/openssl/lib:/home/lixq/toolchains/curl-7.79.1/thirdparty/nghttp3/lib:/home/lixq/toolchains/curl-7.79.1/thirdparty/ngtcp2/lib
-export LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/gcc/lib64:/home/lixq/toolchains/curl-7.79.1/thirdparty/openssl/lib:/home/lixq/toolchains/curl-7.79.1/thirdparty/nghttp3/lib:/home/lixq/toolchains/curl-7.79.1/thirdparty/ngtcp2/lib"
-export PKG_CONFIG_PATH=/home/lixq/toolchains/curl-7.79.1/thirdparty/openssl/lib/pkgconfig:/home/lixq/toolchains/curl-7.79.1/thirdparty/nghttp3/lib/pkgconfig:/home/lixq/toolchains/curl-7.79.1/thirdparty/ngtcp2/lib/pkgconfig
+export LIBRARY_PATH=/home/lixq/toolchains/gcc/lib64:/home/lixq/toolchains/curl-7.80.0/thirdparty/openssl/lib:/home/lixq/toolchains/curl-7.80.0/thirdparty/nghttp3/lib:/home/lixq/toolchains/curl-7.80.0/thirdparty/ngtcp2/lib
+export LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/gcc/lib64:/home/lixq/toolchains/curl-7.80.0/thirdparty/openssl/lib:/home/lixq/toolchains/curl-7.80.0/thirdparty/nghttp3/lib:/home/lixq/toolchains/curl-7.80.0/thirdparty/ngtcp2/lib"
+export PKG_CONFIG_PATH=/home/lixq/toolchains/curl-7.80.0/thirdparty/openssl/lib/pkgconfig:/home/lixq/toolchains/curl-7.80.0/thirdparty/nghttp3/lib/pkgconfig:/home/lixq/toolchains/curl-7.80.0/thirdparty/ngtcp2/lib/pkgconfig
 
-[[ -d /home/lixq/toolchains/src ]] || mkdir -p /home/lixq/toolchains/src
-rm -rf /home/lixq/toolchains/curl-7.79.1
-mkdir -p /home/lixq/toolchains/curl-7.79.1/thirdparty
+[[ -d /home/lixq/src ]] || mkdir -p /home/lixq/src
+rm -rf /home/lixq/toolchains/curl-7.80.0
+mkdir -p /home/lixq/toolchains/curl-7.80.0/thirdparty
 cd /home/lixq/toolchains
 
 # openssl
-if [[ ! -d /home/lixq/toolchains/src/openssl-1.1.1l-quic ]]; then
-    cd /home/lixq/toolchains/src || exit 1
+if [[ ! -d /home/lixq/src/openssl-1.1.1l-quic ]]; then
+    cd /home/lixq/src || exit 1
     if ! git clone --depth 1 -b OpenSSL_1_1_1l+quic https://github.com.cnpmjs.org/quictls/openssl openssl-1.1.1l-quic; then
-        echo "Need /home/lixq/toolchains/src/openssl-1.1.1l-quic (git clone --depth 1 -b OpenSSL_1_1_1l+quic https://github.com/quictls/openssl)"
+        echo "Need /home/lixq/src/openssl-1.1.1l-quic (git clone --depth 1 -b OpenSSL_1_1_1l+quic https://github.com/quictls/openssl)"
         exit 1
     fi
 fi
-cd /home/lixq/toolchains/src/openssl-1.1.1l-quic || exit 1
-./config enable-tls1_3 --prefix=/home/lixq/toolchains/curl-7.79.1/thirdparty/openssl-1.1.1l-quic
+cd /home/lixq/src/openssl-1.1.1l-quic || exit 1
+./config enable-tls1_3 --prefix=/home/lixq/toolchains/curl-7.80.0/thirdparty/openssl-1.1.1l-quic
 make -j4
 make install_sw
-if [[ -d /home/lixq/toolchains/curl-7.79.1/thirdparty/openssl-1.1.1l-quic ]]; then
-    cd /home/lixq/toolchains/curl-7.79.1/thirdparty || exit 1
+if [[ -d /home/lixq/toolchains/curl-7.80.0/thirdparty/openssl-1.1.1l-quic ]]; then
+    cd /home/lixq/toolchains/curl-7.80.0/thirdparty || exit 1
     ln -s openssl-1.1.1l-quic openssl
 fi
 
 # nghttp3
-if [[ ! -d /home/lixq/toolchains/src/nghttp3 ]]; then
-    cd /home/lixq/toolchains/src || exit 1
+if [[ ! -d /home/lixq/src/nghttp3 ]]; then
+    cd /home/lixq/src || exit 1
     if ! git clone https://github.com.cnpmjs.org/ngtcp2/nghttp3; then
-        echo "Need /home/lixq/toolchains/src/nghttp3 (https://github.com/ngtcp2/nghttp3)"
+        echo "Need /home/lixq/src/nghttp3 (https://github.com/ngtcp2/nghttp3)"
         exit 1
     fi
 fi
-cd /home/lixq/toolchains/src/nghttp3 || exit 1
+cd /home/lixq/src/nghttp3 || exit 1
 autoreconf -i
-./configure --prefix=/home/lixq/toolchains/curl-7.79.1/thirdparty/nghttp3 --enable-lib-only
+./configure --prefix=/home/lixq/toolchains/curl-7.80.0/thirdparty/nghttp3 --enable-lib-only
 make -j4 check
 make install
 
 # ngtcp2
-if [[ ! -d /home/lixq/toolchains/src/ngtcp2 ]]; then
-    cd /home/lixq/toolchains/src || exit 1
+if [[ ! -d /home/lixq/src/ngtcp2 ]]; then
+    cd /home/lixq/src || exit 1
     if ! git clone https://github.com.cnpmjs.org/ngtcp2/ngtcp2; then
-        echo "Need /home/lixq/toolchains/src/ngtcp2 (https://github.com/ngtcp2/ngtcp2)"
+        echo "Need /home/lixq/src/ngtcp2 (https://github.com/ngtcp2/ngtcp2)"
         exit 1
     fi
 fi
-cd /home/lixq/toolchains/src/ngtcp2 || exit 1
+cd /home/lixq/src/ngtcp2 || exit 1
 autoreconf -i
-./configure --prefix=/home/lixq/toolchains/curl-7.79.1/thirdparty/ngtcp2
+./configure --prefix=/home/lixq/toolchains/curl-7.80.0/thirdparty/ngtcp2
 make -j4 check
 make install
 
 # curl
-if [[ ! -f /home/lixq/toolchains/src/curl-7.79.1.tar.bz2 ]]; then
-    cd /home/lixq/toolchains/src || exit 1
-    if ! wget https://github.com.cnpmjs.org/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.bz2; then
-        echo "Need /home/lixq/toolchains/src/curl-7.79.1.tar.bz2 (https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.bz2)"
+if [[ ! -f /home/lixq/src/curl-7.80.0.tar.bz2 ]]; then
+    cd /home/lixq/src || exit 1
+    if ! wget https://github.com.cnpmjs.org/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.bz2; then
+        echo "Need /home/lixq/src/curl-7.80.0.tar.bz2 (https://github.com/curl/curl/releases/download/curl-7.80.0/curl-7.80.0.tar.bz2)"
         exit 1
     fi
 fi
-cd /home/lixq/toolchains/src || exit 1
-rm -rf curl-7.79.1
-tar -xvf curl-7.79.1.tar.bz2
-cd /home/lixq/toolchains/src/curl-7.79.1 || exit 1
-./configure --prefix=/home/lixq/toolchains/curl-7.79.1 --with-openssl --with-libssh2 --with-brotli --with-zstd --with-gssapi --with-libidn2 --enable-ldap --enable-ldaps --with-librtmp --with-nghttp2 --with-ngtcp2 || exit 1
+cd /home/lixq/src || exit 1
+rm -rf curl-7.80.0
+tar -xvf curl-7.80.0.tar.bz2
+cd /home/lixq/src/curl-7.80.0 || exit 1
+./configure --prefix=/home/lixq/toolchains/curl-7.80.0 --with-openssl --with-libssh2 --with-brotli --with-zstd --with-gssapi --with-libidn2 --enable-ldap --enable-ldaps --with-librtmp --with-nghttp2 --with-ngtcp2 || exit 1
 make || exit 1
 make install || exit 1
-if [[ -d /home/lixq/toolchains/curl-7.79.1 ]]; then
+if [[ -d /home/lixq/toolchains/curl-7.80.0 ]]; then
     cd /home/lixq/toolchains || exit 1
-    ln -s curl-7.79.1 curl
+    rm curl
+    ln -s curl-7.80.0 curl
 fi
--- buildsh/build_git.sh
+++ buildsh/build_git.sh
@@ -2,24 +2,24 @@
 
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
-[[ -d /home/lixq/toolchains/src ]] || mkdir -p /home/lixq/toolchains/src
+[[ -d /home/lixq/src ]] || mkdir -p /home/lixq/src
 
-if [[ ! -f /home/lixq/toolchains/src/git-2.33.1.tar.gz ]]; then
-    if ! wget https://github.com.cnpmjs.org/git/git/archive/refs/tags/v2.33.1.tar.gz -O /home/lixq/toolchains/src/git-2.33.1.tar.gz; then
-        rm -f /home/lixq/toolchains/src/git-2.33.1.tar.gz*
-        echo "Need /home/lixq/toolchains/src/git-2.33.1.tar.gz (https://github.com/git/git/archive/refs/tags/v2.33.1.tar.gz)"
+if [[ ! -f /home/lixq/src/git-2.34.1.tar.gz ]]; then
+    if ! wget https://github.com.cnpmjs.org/git/git/archive/refs/tags/v2.34.1.tar.gz -O /home/lixq/src/git-2.34.1.tar.gz; then
+        rm -f /home/lixq/src/git-2.34.1.tar.gz*
+        echo "Need /home/lixq/src/git-2.34.1.tar.gz (https://github.com/git/git/archive/refs/tags/v2.34.1.tar.gz)"
         exit 1
     fi
 fi
 
-cd /home/lixq/toolchains/src || exit 1
-rm -rf git-2.33.1 /home/lixq/toolchains/git-2.33.1
-tar -xvf git-2.33.1.tar.gz
-cd /home/lixq/toolchains/src/git-2.33.1 || exit 1
-make prefix=/home/lixq/toolchains/git-2.33.1 || exit 1
-make prefix=/home/lixq/toolchains/git-2.33.1 install || exit 1
-if [[ -d /home/lixq/toolchains/git-2.33.1 ]]; then
+cd /home/lixq/src || exit 1
+rm -rf git-2.34.1 /home/lixq/toolchains/git-2.34.1
+tar -xvf git-2.34.1.tar.gz
+cd /home/lixq/src/git-2.34.1 || exit 1
+make prefix=/home/lixq/toolchains/git-2.34.1 || exit 1
+make prefix=/home/lixq/toolchains/git-2.34.1 install || exit 1
+if [[ -d /home/lixq/toolchains/git-2.34.1 ]]; then
     cd /home/lixq/toolchains || exit 1
     rm git
-    ln -s git-2.33.1 git
+    ln -s git-2.34.1 git
 fi
