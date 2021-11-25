821788c8b6ace4d21c16b2c83dab4bcb68541b65
Li Xiaqin <lixiaqin0301@gmail.com>
2021-11-25 13:40:44 +0800

    Build bear

--- buildsh/build_bear.sh
+++ buildsh/build_bear.sh
@@ -2,17 +2,18 @@
 
 [[ -d /home/lixq/src ]] || mkdir -p /home/lixq/src
 
-if [[ ! -f /home/lixq/src/Bear-3.0.16.tar.gz ]]; then
-    if ! wget https://github.com.cnpmjs.org/rizsotto/Bear/archive/refs/tags/3.0.16.tar.gz -O /home/lixq/src/Bear-3.0.16.tar.gz; then
-        rm -f /home/lixq/src/Bear-3.0.16.tar.gz*
-        echo "Need /home/lixq/src/Bear-3.0.16.tar.gz (https://github.com.cnpmjs.org/rizsotto/Bear/archive/refs/tags/3.0.16.tar.gz)"
+if [[ ! -f /home/lixq/src/Bear-3.0.17.tar.gz ]]; then
+    if ! wget https://github.com.cnpmjs.org/rizsotto/Bear/archive/refs/tags/3.0.17.tar.gz -O /home/lixq/src/Bear-3.0.17.tar.gz; then
+        rm -f /home/lixq/src/Bear-3.0.17.tar.gz*
+        echo "Need /home/lixq/src/Bear-3.0.17.tar.gz (https://github.com.cnpmjs.org/rizsotto/Bear/archive/refs/tags/3.0.17.tar.gz)"
         exit 1
     fi
 fi
 
 cd /home/lixq/src || exit 1
-rm -rf Bear-3.0.16
-tar -xvf Bear-3.0.16.tar.gz
+rm -rf Bear-3.0.17
+tar -xvf Bear-3.0.17.tar.gz
+
 # googletest
 if [[ ! -f /home/lixq/src/googletest-1.11.0.tar.gz ]]; then
     if ! wget https://github.com.cnpmjs.org/google/googletest/archive/release-1.11.0.tar.gz -O /home/lixq/src/googletest-1.11.0.tar.gz; then
@@ -21,18 +22,18 @@ if [[ ! -f /home/lixq/src/googletest-1.11.0.tar.gz ]]; then
         exit 1
     fi
 fi
-[[ -d /home/lixq/src/Bear-3.0.16/build/subprojects/Download/googletest_dependency ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build/subprojects/Download/googletest_dependency
-cp -f /home/lixq/src/googletest-1.11.0.tar.gz /home/lixq/src/Bear-3.0.16/build/subprojects/Download/googletest_dependency/release-1.11.0.tar.gz
+[[ -d /home/lixq/src/Bear-3.0.17/build/subprojects/Download/googletest_dependency ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build/subprojects/Download/googletest_dependency
+cp -f /home/lixq/src/googletest-1.11.0.tar.gz /home/lixq/src/Bear-3.0.17/build/subprojects/Download/googletest_dependency/release-1.11.0.tar.gz
 # json
-if [[ ! -f /home/lixq/src/json-v3.10.2.tar.gz ]]; then
-    if ! wget https://github.com.cnpmjs.org/nlohmann/json/archive/v3.10.2.tar.gz -O /home/lixq/src/json-v3.10.2.tar.gz; then
-        rm /home/lixq/src/json-v3.10.2.tar.gz*
-        echo "Need /home/lixq/src/json-v3.10.2.tar.gz (https://github.com.cnpmjs.org/nlohmann/json/archive/v3.10.2.tar.gz)"
+if [[ ! -f /home/lixq/src/json-v3.10.4.tar.gz ]]; then
+    if ! wget https://github.com.cnpmjs.org/nlohmann/json/archive/v3.10.4.tar.gz -O /home/lixq/src/json-v3.10.4.tar.gz; then
+        rm /home/lixq/src/json-v3.10.4.tar.gz*
+        echo "Need /home/lixq/src/json-v3.10.4.tar.gz (https://github.com.cnpmjs.org/nlohmann/json/archive/v3.10.4.tar.gz)"
         exit 1
     fi
 fi
-[[ -d /home/lixq/src/Bear-3.0.16/build/subprojects/Download/nlohmann_json_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build/subprojects/Download/nlohmann_json_dependency/
-cp -f /home/lixq/src/json-v3.10.2.tar.gz /home/lixq/src/Bear-3.0.16/build/subprojects/Download/nlohmann_json_dependency/v3.10.2.tar.gz
+[[ -d /home/lixq/src/Bear-3.0.17/build/subprojects/Download/nlohmann_json_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build/subprojects/Download/nlohmann_json_dependency/
+cp -f /home/lixq/src/json-v3.10.4.tar.gz /home/lixq/src/Bear-3.0.17/build/subprojects/Download/nlohmann_json_dependency/v3.10.4.tar.gz
 # fmt
 if [[ ! -f /home/lixq/src/fmt-8.0.1.tar.gz ]]; then
     if ! wget https://github.com.cnpmjs.org/fmtlib/fmt/archive/8.0.1.tar.gz -O /home/lixq/src/fmt-8.0.1.tar.gz; then
@@ -41,8 +42,8 @@ if [[ ! -f /home/lixq/src/fmt-8.0.1.tar.gz ]]; then
         exit 1
     fi
 fi
-[[ -d /home/lixq/src/Bear-3.0.16/build/subprojects/Download/fmt_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build/subprojects/Download/fmt_dependency/
-cp -f /home/lixq/src/fmt-8.0.1.tar.gz /home/lixq/src/Bear-3.0.16/build/subprojects/Download/fmt_dependency/8.0.1.tar.gz
+[[ -d /home/lixq/src/Bear-3.0.17/build/subprojects/Download/fmt_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build/subprojects/Download/fmt_dependency/
+cp -f /home/lixq/src/fmt-8.0.1.tar.gz /home/lixq/src/Bear-3.0.17/build/subprojects/Download/fmt_dependency/8.0.1.tar.gz
 # spdlog
 if [[ ! -f /home/lixq/src/spdlog-v1.9.2.tar.gz ]]; then
     if ! wget https://github.com.cnpmjs.org/gabime/spdlog/archive/v1.9.2.tar.gz -O /home/lixq/src/spdlog-v1.9.2.tar.gz; then
@@ -51,45 +52,45 @@ if [[ ! -f /home/lixq/src/spdlog-v1.9.2.tar.gz ]]; then
         exit 1
     fi
 fi
-[[ -d /home/lixq/src/Bear-3.0.16/build/subprojects/Download/spdlog_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build/subprojects/Download/spdlog_dependency/
-cp -f /home/lixq/src/spdlog-v1.9.2.tar.gz /home/lixq/src/Bear-3.0.16/build/subprojects/Download/spdlog_dependency/v1.9.2.tar.gz
+[[ -d /home/lixq/src/Bear-3.0.17/build/subprojects/Download/spdlog_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build/subprojects/Download/spdlog_dependency/
+cp -f /home/lixq/src/spdlog-v1.9.2.tar.gz /home/lixq/src/Bear-3.0.17/build/subprojects/Download/spdlog_dependency/v1.9.2.tar.gz
 # grpc
-if [[ ! -f /home/lixq/src/grpc-v1.36.4.tar.gz ]]; then
+if [[ ! -f /home/lixq/src/grpc-v1.41.1.tar.gz ]]; then
     cd /home/lixq/src || exit 1
-    if [[ ! -d /home/lixq/src/grpc-v1.36.4 ]]; then
-        if ! git clone --depth 1 https://github.com.cnpmjs.org.cnpmjs.org/grpc/grpc -b v1.36.4 grpc-v1.36.4; then
-            echo "Need /home/lixq/src/grpc-v1.36.4 (https://github.com.cnpmjs.org/grpc/grpc)"
+    if [[ ! -d /home/lixq/src/grpc-v1.41.1 ]]; then
+        if ! git clone --depth 1 https://github.com.cnpmjs.org/grpc/grpc -b v1.41.1 grpc-v1.41.1; then
+            echo "Need /home/lixq/src/grpc-v1.41.1 (https://github.com.cnpmjs.org/grpc/grpc)"
             exit 1
         fi
-        for d in /home/lixq/src/grpc-v1.36.4 \
-            /home/lixq/src/grpc-v1.36.4/third_party/bloaty \
-            /home/lixq/src/grpc-v1.36.4/third_party/protobuf; do
+        for d in /home/lixq/src/grpc-v1.41.1 \
+            /home/lixq/src/grpc-v1.41.1/third_party/bloaty \
+            /home/lixq/src/grpc-v1.41.1/third_party/protobuf; do
             cd "$d" || exit 1
             pwd
-            sed -i 's;github.com.cnpmjs.org/;github.com.cnpmjs.org.cnpmjs.org/;' .gitmodules
+            sed -i 's;github.com.cnpmjs.org/;github.com.cnpmjs.org/;' .gitmodules
             git submodule update --init
         done
     fi
-    tar -czvf grpc-v1.36.4.tar.gz grpc-v1.36.4
+    tar -czvf grpc-v1.41.1.tar.gz grpc-v1.41.1
 fi
-n=$(awk '/ExternalProject_Add/{print FNR}' /home/lixq/src/Bear-3.0.16/third_party/grpc/CMakeLists.txt)
-sed -i "$((n+1)),$((n+7))c\            URL\n                https://github.com.cnpmjs.org.cnpmjs.org/grpc/grpc/archive/refs/tags/v1.36.4.tar.gz\n            URL_HASH\n                MD5=$(md5sum /home/lixq/src/grpc-v1.36.4.tar.gz | awk '{print $1}')\n            DOWNLOAD_NO_PROGRESS\n                1" /home/lixq/src/Bear-3.0.16/third_party/grpc/CMakeLists.txt
-[[ -d /home/lixq/src/Bear-3.0.16/build/subprojects/Download/grpc_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build/subprojects/Download/grpc_dependency/
-cp -f /home/lixq/src/grpc-v1.36.4.tar.gz /home/lixq/src/Bear-3.0.16/build/subprojects/Download/grpc_dependency/v1.36.4.tar.gz
+n=$(awk '/ExternalProject_Add/{print FNR}' /home/lixq/src/Bear-3.0.17/third_party/grpc/CMakeLists.txt)
+sed -i "$((n+1)),$((n+7))c\            URL\n                https://github.com.cnpmjs.org/grpc/grpc/archive/refs/tags/v1.41.1.tar.gz\n            URL_HASH\n                MD5=$(md5sum /home/lixq/src/grpc-v1.41.1.tar.gz | awk '{print $1}')\n            DOWNLOAD_NO_PROGRESS\n                1" /home/lixq/src/Bear-3.0.17/third_party/grpc/CMakeLists.txt
+[[ -d /home/lixq/src/Bear-3.0.17/build/subprojects/Download/grpc_dependency/ ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build/subprojects/Download/grpc_dependency/
+cp -f /home/lixq/src/grpc-v1.41.1.tar.gz /home/lixq/src/Bear-3.0.17/build/subprojects/Download/grpc_dependency/v1.41.1.tar.gz
 
-[[ -d /home/lixq/src/Bear-3.0.16/build ]] || mkdir -p /home/lixq/src/Bear-3.0.16/build
-cd /home/lixq/src/Bear-3.0.16/build || exit 1
-rm -rf /home/lixq/toolchains/Bear-3.0.16
+[[ -d /home/lixq/src/Bear-3.0.17/build ]] || mkdir -p /home/lixq/src/Bear-3.0.17/build
+cd /home/lixq/src/Bear-3.0.17/build || exit 1
+rm -rf /home/lixq/toolchains/Bear-3.0.17
 export CC=/home/lixq/toolchains/gcc/bin/gcc
 export CXX=/home/lixq/toolchains/gcc/bin/g++
 export CPP=/home/lixq/toolchains/gcc/bin/cpp
 export LIBRARY_PATH=/home/lixq/toolchains/gcc/lib64
 export LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/gcc/lib64"
-cmake -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/Bear-3.0.16 ..
+cmake -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/Bear-3.0.17 ..
 make || exit 1
 make install || exit 1
-if [[ -d /home/lixq/toolchains/Bear-3.0.16 ]]; then
+if [[ -d /home/lixq/toolchains/Bear-3.0.17 ]]; then
     cd /home/lixq/toolchains || exit 1
     rm -f Bear
-    ln -s Bear-3.0.16 Bear
+    ln -s Bear-3.0.17 Bear
 fi
