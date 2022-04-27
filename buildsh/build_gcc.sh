167d2d3f46e1c2815e7bfe5669970adf819a7152
Li Xiaqin <lixiaqin0301@gmail.com>
2022-04-27 14:57:26 +0800

    gcc 11.3.0

--- buildsh/build_gcc.sh
+++ buildsh/build_gcc.sh
@@ -1,62 +1,64 @@
 #!/bin/bash
 
+ver=11.3.0
+
 gmp='gmp-6.1.0.tar.bz2'
 mpfr='mpfr-3.1.6.tar.bz2'
 mpc='mpc-1.0.3.tar.gz'
 isl='isl-0.18.tar.bz2'
 
-[[ -d /home/lixq/toolchains/src ]] || mkdir -p /home/lixq/toolchains/src
+[[ -d /home/lixq/src ]] || mkdir -p /home/lixq/src
 
-if [[ ! -f /home/lixq/toolchains/src/gcc-11.2.0.tar.gz ]]; then
-    if ! wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.gz -O /home/lixq/toolchains/src/gcc-11.2.0.tar.gz; then
-        rm -f /home/lixq/toolchains/src/gcc-11.2.0.tar.gz*
-        echo "Need /home/lixq/toolchains/src/gcc-11.2.0.tar.gz (http://mirrors.ustc.edu.cn/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.gz)"
+if [[ ! -f /home/lixq/src/gcc-${ver}.tar.gz ]]; then
+    if ! wget http://mirrors.ustc.edu.cn/gnu/gcc/gcc-${ver}/gcc-${ver}.tar.gz -O /home/lixq/src/gcc-${ver}.tar.gz; then
+        rm -f /home/lixq/src/gcc-${ver}.tar.gz*
+        echo "Need /home/lixq/src/gcc-${ver}.tar.gz (http://mirrors.ustc.edu.cn/gnu/gcc/gcc-${ver}/gcc-${ver}.tar.gz)"
         exit 1
     fi
 fi
-if [[ ! -f /home/lixq/toolchains/src/$gmp ]]; then
-    if ! wget http://mirrors.ustc.edu.cn/gnu/gmp/$gmp -O /home/lixq/toolchains/src/$gmp; then
-        rm -f /home/lixq/toolchains/src/${gmp}*
-        echo "Need /home/lixq/toolchains/src/$gmp (http://mirrors.ustc.edu.cn/gnu/gmp/$gmp)"
+if [[ ! -f /home/lixq/src/${gmp} ]]; then
+    if ! wget http://mirrors.ustc.edu.cn/gnu/gmp/${gmp} -O /home/lixq/src/${gmp}; then
+        rm -f /home/lixq/src/${gmp}*
+        echo "Need /home/lixq/src/${gmp} (http://mirrors.ustc.edu.cn/gnu/gmp/${gmp})"
         exit 1
     fi
 fi
-if [[ ! -f /home/lixq/toolchains/src/$mpfr ]]; then
-    if ! wget http://mirrors.ustc.edu.cn/gnu/mpfr/$mpfr -O /home/lixq/toolchains/src/$mpfr; then
-        echo "Need /home/lixq/toolchains/src/$mpfr (http://mirrors.ustc.edu.cn/gnu/mpfr/$mpfr)"
+if [[ ! -f /home/lixq/src/${mpfr} ]]; then
+    if ! wget http://mirrors.ustc.edu.cn/gnu/mpfr/${mpfr} -O /home/lixq/src/${mpfr}; then
+        echo "Need /home/lixq/src/${mpfr} (http://mirrors.ustc.edu.cn/gnu/mpfr/${mpfr})"
         exit 1
     fi
 fi
-if [[ ! -f /home/lixq/toolchains/src/$mpc ]]; then
-    if ! wget http://mirrors.ustc.edu.cn/gnu/mpc/$mpc -O /home/lixq/toolchains/src/$mpc; then
-        echo "Need /home/lixq/toolchains/src/$mpc (http://mirrors.ustc.edu.cn/gnu/mpc/$mpc)"
+if [[ ! -f /home/lixq/src/${mpc} ]]; then
+    if ! wget http://mirrors.ustc.edu.cn/gnu/mpc/${mpc} -O /home/lixq/src/${mpc}; then
+        echo "Need /home/lixq/src/${mpc} (http://mirrors.ustc.edu.cn/gnu/mpc/${mpc})"
         exit 1
     fi
 fi
-if [[ ! -f /home/lixq/toolchains/src/$isl ]]; then
-    if ! wget https://gcc.gnu.org/pub/gcc/infrastructure/$isl -O /home/lixq/toolchains/src/$isl; then
-        echo "Need /home/lixq/toolchains/src/$isl (https://gcc.gnu.org/pub/gcc/infrastructure/$isl)"
+if [[ ! -f /home/lixq/src/${isl} ]]; then
+    if ! wget https://gcc.gnu.org/pub/gcc/infrastructure/${isl} -O /home/lixq/src/${isl}; then
+        echo "Need /home/lixq/src/${isl} (https://gcc.gnu.org/pub/gcc/infrastructure/${isl})"
         exit 1
     fi
 fi
 
-cd /home/lixq/toolchains/src || exit 1
-rm -rf gcc-11.2.0
-tar -xvf gcc-11.2.0.tar.gz
-cd /home/lixq/toolchains/src/gcc-11.2.0 || exit 1
-cp /home/lixq/toolchains/src/$gmp .
-cp /home/lixq/toolchains/src/$mpfr .
-cp /home/lixq/toolchains/src/$mpc .
-cp /home/lixq/toolchains/src/$isl .
+cd /home/lixq/src || exit 1
+rm -rf gcc-${ver}
+tar -xvf gcc-${ver}.tar.gz
+cd /home/lixq/src/gcc-${ver} || exit 1
+cp /home/lixq/src/${gmp} .
+cp /home/lixq/src/${mpfr} .
+cp /home/lixq/src/${mpc} .
+cp /home/lixq/src/${isl} .
 ./contrib/download_prerequisites
-mkdir -p /home/lixq/toolchains/src/gcc-11.2.0/build
-cd /home/lixq/toolchains/src/gcc-11.2.0/build || exit 1
-rm -rf /home/lixq/toolchains/gcc-11.2.0
-../configure --prefix=/home/lixq/toolchains/gcc-11.2.0 --disable-multilib
+mkdir -p /home/lixq/src/gcc-${ver}/build
+cd /home/lixq/src/gcc-${ver}/build || exit 1
+rm -rf /home/lixq/toolchains/gcc-${ver}
+../configure --prefix=/home/lixq/toolchains/gcc-${ver} --disable-multilib
 make
 make install
-if [[ -d /home/lixq/toolchains/gcc-11.2.0 ]]; then
+if [[ -d /home/lixq/toolchains/gcc-${ver} ]]; then
     cd /home/lixq/toolchains || exit 1
     rm -f gcc
-    ln -s gcc-11.2.0 gcc
+    ln -s gcc-${ver} gcc
 fi
