fab3880ea9f146f71711a0d1b707bd711f255907
Li Xiaqin <lixiaqin0301@gmail.com>
2022-04-28 09:23:24 +0800

    build llvm

--- buildsh/build_llvm.sh
+++ buildsh/build_llvm.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
 
-if [[ ! -f /home/lixq/toolchains/src/llvm-13.0.0.src.tar.xz ]]; then
-    if ! wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/llvm-13.0.0.src.tar.xz -O /home/lixq/toolchains/src/llvm-13.0.0.src.tar.xz; then
-        rm -f /home/lixq/toolchains/src/llvm-13.0.0.src.tar.xz*
-        echo "Need /home/lixq/toolchains/src/llvm-13.0.0.src.tar.xz (https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/llvm-13.0.0.src.tar.xz)"
+ver=14.0.1
+
+if [[ ! -f /home/lixq/src/llvm-${ver}.src.tar.xz ]]; then
+    if ! wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${ver}/llvm-${ver}.src.tar.xz -O /home/lixq/src/llvm-${ver}.src.tar.xz; then
+        rm -f /home/lixq/src/llvm-${ver}.src.tar.xz*
+        echo "Need /home/lixq/src/llvm-${ver}.src.tar.xz (https://github.com/llvm/llvm-project/releases/download/llvmorg-${ver}/llvm-${ver}.src.tar.xz)"
         exit 1
     fi
 fi
 
-if [[ ! -f /home/lixq/toolchains/src/clang-13.0.0.src.tar.xz ]]; then
-    if ! wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang-13.0.0.src.tar.xz -O /home/lixq/toolchains/src/clang-13.0.0.src.tar.xz; then
-        rm -f /home/lixq/toolchains/src/clang-13.0.0.src.tar.xz*
-        echo "Need /home/lixq/toolchains/src/llvm-13.0.0.src.tar.xz (https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang-13.0.0.src.tar.xz)"
+if [[ ! -f /home/lixq/src/clang-${ver}.src.tar.xz ]]; then
+    if ! wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${ver}/clang-${ver}.src.tar.xz -O /home/lixq/src/clang-${ver}.src.tar.xz; then
+        rm -f /home/lixq/src/clang-${ver}.src.tar.xz*
+        echo "Need /home/lixq/src/llvm-${ver}.src.tar.xz (https://github.com/llvm/llvm-project/releases/download/llvmorg-${ver}/clang-${ver}.src.tar.xz)"
         exit 1
     fi
 fi
 
-cd /home/lixq/toolchains/src || exit 1
+cd /home/lixq/src || exit 1
 rm -rf llvm
-tar -xvf llvm-13.0.0.src.tar.xz
-mv llvm-13.0.0.src llvm
+tar -xvf llvm-${ver}.src.tar.xz
+mv llvm-${ver}.src llvm
 
-cd /home/lixq/toolchains/src/llvm/tools || exit 1
-tar -xvf /home/lixq/toolchains/src/clang-13.0.0.src.tar.xz
-mv clang-13.0.0.src clang
+cd /home/lixq/src/llvm/tools || exit 1
+tar -xvf /home/lixq/src/clang-${ver}.src.tar.xz
+mv clang-${ver}.src clang
 
-mkdir /home/lixq/toolchains/src/llvm/build
-cd /home/lixq/toolchains/src/llvm/build || exit 1
+mkdir /home/lixq/src/llvm/build
+cd /home/lixq/src/llvm/build || exit 1
 
-rm -rf /home/lixq/toolchains/llvm-13.0.0
+rm -rf /home/lixq/toolchains/llvm-${ver}
 export CPP=/home/lixq/toolchains/gcc/bin/cpp
 /home/lixq/toolchains/cmake/bin/cmake \
     -DCMAKE_C_COMPILER=/home/lixq/toolchains/gcc/bin/gcc \
     -DCMAKE_CXX_COMPILER=/home/lixq/toolchains/gcc/bin/g++ \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/llvm-13.0.0 \
+    -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/llvm-${ver} \
     -DLLVM_TARGETS_TO_BUILD=X86 \
     -DLLVM_BUILD_LLVM_DYLIB=1 \
-    -DLLVM_OPTIMIZED_TABLEGEN=1 \
+    -DLLVM_INCLUDE_BENCHMARKS=0 \
     -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,/home/lixq/toolchains/gcc/lib64 -L/home/lixq/toolchains/gcc/lib64" \
     ..
 make
 make install
+#   -DLLVM_OPTIMIZED_TABLEGEN=1 \
 
-if [[ -d /home/lixq/toolchains/llvm-13.0.0 ]]; then
+if [[ -d /home/lixq/toolchains/llvm-${ver} ]]; then
     cd /home/lixq/toolchains || exit 1
     rm -f llvm
-    ln -s llvm-13.0.0 llvm
+    ln -s llvm-${ver} llvm
 fi
