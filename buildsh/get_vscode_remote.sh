09ac239bfe4d62938ced2ec79397e0038f2f055e
lixq <lixq@wangsu.com>
2024-06-17 10:29:25 +0800

    vscode

--- buildsh/get_vscode_remote.sh
+++ buildsh/get_vscode_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-version=1.90.0
-commit=89de5a8d4d6205e5b11647eb6a74844ca23d2573
+version=1.90.1
+commit=611f9bfce64f25108829dd295f54a6894e87339d
 
 [[ -d /share-rd/lixq/vscode ]] || mkdir -p /share-rd/lixq/vscode
 if [[ ! -f "/share-rd/lixq/vscode/vscode-server-${version}.linux-x64.tar.gz" ]]; then
