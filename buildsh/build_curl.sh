090e777f7c38f51655b79b06f6ae233e6e7fc080
lixq <lixq@wangsu.com>
2024-06-19 11:29:46 +0800

    build curl

--- buildsh/build_curl.sh
+++ buildsh/build_curl.sh
@@ -32,8 +32,7 @@ tar -xf /home/lixq/35share-rd/src/ngtcp2.tar.xz
 cd /home/lixq/src/ngtcp2 || exit 1
 autoreconf -fi || exit 1
 [[ -d /home/lixq/toolchains/curl/libs/ngtcp2 ]] || mkdir -p /home/lixq/toolchains/curl/libs/ngtcp2
-./configure PKG_CONFIG_PATH=/home/lixq/toolchains/curl/libs/openssl/lib64/pkgconfig:/home/lixq/toolchains/curl/libs/nghttp3/lib64/pkgconfig LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/lib
-s/openssl/lib64" --prefix=/home/lixq/toolchains/curl/libs/ngtcp2 --enable-lib-only || exit 1
+./configure PKG_CONFIG_PATH=/home/lixq/toolchains/curl/libs/openssl/lib64/pkgconfig:/home/lixq/toolchains/curl/libs/nghttp3/lib/pkgconfig LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/libs/openssl/lib64" --prefix=/home/lixq/toolchains/curl/libs/ngtcp2 --enable-lib-only || exit 1
 make || exit 1
 make install || exit 1
 
@@ -42,7 +41,6 @@ rm -rfv curl
 tar -xf /home/lixq/35share-rd/src/curl.tar.xz
 cd /home/lixq/src/curl || exit 1
 autoreconf -fi || exit 1
-LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/libs/openssl/lib64" ./configure --prefix=/home/lixq/toolchains/curl --with-openssl=/home/lixq/toolchains/curl/libs/openssl --with-nghttp3=/home
-/lixq/toolchains/curl/libs/nghttp3 --with-ngtcp2=/home/lixq/toolchains/curl/libs/ngtcp2 || exit 1
+LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/libs/openssl/lib64" ./configure --prefix=/home/lixq/toolchains/curl --with-openssl=/home/lixq/toolchains/curl/libs/openssl --with-nghttp3=/home/lixq/toolchains/curl/libs/nghttp3 --with-ngtcp2=/home/lixq/toolchains/curl/libs/ngtcp2 || exit 1
 make || exit 1
 make install || exit 1
