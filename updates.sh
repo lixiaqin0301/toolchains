1230462199d834bd8dec1e9bc838bb0b6cd41483
lixq <lixq@wangsu.com>
2024-07-10 14:48:52 +0800

    updates

--- updates.sh
+++ updates.sh
@@ -1,104 +1,50 @@
 #!/bin/bash
 
-function recover() {
-    [[ -f ${sdir}/config.vim ]] && mv "${sdir}/config.vim" "${tdir}/SpaceVim.d/autoload/config.vim"
-}
-trap recover EXIT
-
-if [[ -d /home/lixq/toolchains ]]; then
-    tdir=/home/lixq/toolchains
-    sdir=/home/lixq/src
-    [[ -d "${sdir}" ]] || mkdir -p "${sdir}"
-    cd "${sdir}" || exit 1
-    rm -f nvim-linux64.tar.gz nvim-linux64.tar.gz.*
-    #until wget -T 10 -c "https://github.com/neovim/neovim-releases/releases/download/nightly/nvim-linux64.tar.gz"; do
-    until wget -T 10 -c "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"; do
-        sleep 1
-    done
-    cd "${tdir}" || exit 1
-    rm -rf nvim-*
-    tar -xf "${sdir}/nvim-linux64.tar.gz"
-else
-    tdir=~/toolchains
-    sdir=~/src
-    [[ -d "${sdir}" ]] || mkdir -p "${sdir}"
-    cp "${tdir}/SpaceVim.d/autoload/config.vim" "${sdir}"
-    cd "${tdir}" || exit 1
-    git restore .
-    cd "${sdir}" || exit 1
-    rm -f nvim-macos-x86_64.tar.gz nvim-macos-x86_64.tar.gz.*
-    until wget -T 10 -c "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"; do
-        sleep 1
-    done
-    cd "${tdir}" || exit 1
-    rm -rf nvim-*
-    tar -xf "${sdir}/nvim-macos-x86_64.tar.gz"
-fi
-
-cd "${sdir}" || exit 1
-if [[ -d /home/lixq/src/neovim ]]; then
-    cd /home/lixq/src/neovim || exit 1
-    git restore .
-    until git pull; do
-        sleep 1
-    done
-    make install
-fi
-
-cd "${tdir}" || exit 1
+cd "/home/lixq/toolchains" || exit 1
 pwd
-until git pull; do
-    sleep 1
+for _ in {0..9}; do
+    git pull && break
 done
 
-while read -r d; do
-    cd "$(dirname "${d}")" || continue
+sudo apt update -y
+sudo apt full-upgrade -y
+
+for d in /home/lixq/toolchains/*/.git; do
+    cd "$(dirname "${d}")" || exit
     pwd
-    git checkout "$(git branch -la | awk '{print $1}' | grep -E 'remotes/origin/(master|hg|main)' | head -n 1 | cut -b 16- || true)"
-    until git pull; do
-        sleep 1
+    git restore .
+    git clean -fdx
+    for _ in {0..9}; do
+        git pull && break
     done
-    until git submodule update --init --recursive; do
-        sleep 1
-    done
-done < <(find "${tdir}"/*/ -name .git || true)
-
-cd ~ || exit 1
-if command -v brew; then
-    until brew update; do
-        sleep 1
-    done
-    until brew upgrade; do
-        sleep 1
-    done
-elif command -v apt; then
-    sudo apt update -y
-    sudo apt full-upgrade -y
-elif command -v yum; then
-    yum clean all
-    yum update -y --skip-broken
-    yum upgrade -y --skip-broken
-fi
-
-if command -v rustup; then
-    rustup update
-fi
-
-cd "${tdir}/github.com/Valloric/YouCompleteMe" || exit 1
-if [[ -d /home/lixq/toolchains/llvm/include ]]; then
-    export CPATH=/home/lixq/toolchains/llvm/include
-    export LIBRARY_PATH=/home/lixq/toolchains/llvm/lib
-    export LD_LIBRARY_PATH=/home/lixq/toolchains/llvm/lib
-    export LD_RUN_PATH=/home/lixq/toolchains/llvm/lib
-    export LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/llvm/lib:/home/lixq/toolchains/Anaconda3/lib"
-fi
-until python3 install.py --system-libclang --clang-completer --go-completer --verbose; do
-    sleep 1
 done
-if [[ -d /usr/local/Cellar/llvm ]]; then
-    cd "${tdir}/github.com/Valloric/YouCompleteMe/third_party/ycmd/third_party/clang/lib" || exit 1
-    for f in /usr/local/Cellar/llvm/*/lib/libclang.dylib; do
-        ln -sf "${f}" .
-        break
-    done
+sed -i 's/print("/ngx.log(ngx.ERR, "/' /home/lixq/toolchains/LuaPanda/Debugger/LuaPanda.lua
+
+[[ -d "/home/lixq/src" ]] || mkdir -p "/home/lixq/src"
+cd "/home/lixq/src" || exit 1
+rm -f nvim-linux64.tar.gz nvim-linux64.tar.gz.*
+for _ in {0..9}; do
+    wget -c "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz" && break
+    rm -f nvim-linux64.tar.gz nvim-linux64.tar.gz.*
+done
+if [[ -f /home/lixq/src/nvim-linux64.tar.gz ]]; then
+    cd "/home/lixq/toolchains" || exit 1
+    rm -rf nvim-*
+    tar -xf /home/lixq/src/nvim-linux64.tar.gz
 fi
+
+sudo rm -rf /home/lixq/toolchains/github.com /tmp/t
+mkdir -p /home/lixq/toolchains/github.com /tmp/t
+touch /tmp/t/t.c /tmp/t/t.py /tmp/t/t.sh /tmp/t/t.lua /tmp/t/t.go
+nvim /tmp/t/t.*
+cd /home/lixq/toolchains/github.com/Valloric/YouCompleteMe/ || exit 1
+for _ in {0..9}; do
+    git submodule update --init --recursive && break
+done
+for _ in {0..9}; do
+    python3 install.py --clang-completer --system-libclang --go-completer --verbose && break
+done
+
+cd /home/lixq || exit 1
+sudo rm -rf /mnt/d/toolchains.tar
+tar -cf /mnt/d/toolchains.tar toolchains
