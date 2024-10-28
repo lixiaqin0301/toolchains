317527837e5c62108a122ede3f4cd8385c4b39b4
lixq <lixq@wangsu.com>
2024-10-28 14:52:57 +0800

    curl http2

--- buildsh/build_curl.sh
+++ buildsh/build_curl.sh
@@ -3,11 +3,21 @@
 # https://curl.se/docs/http3.html
 
 ver=8.10.1
+nghttp2ver=1.64.0
 
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 . /opt/rh/devtoolset-11/enable
 
 [[ -d /home/lixq/src ]] || mkdir /home/lixq/src
+
+cd /home/lixq/src || exit 1
+rm -rfv nghttp2-${nghttp2ver}
+tar -xf /home/lixq/35share-rd/src/nghttp2-${nghttp2ver}.tar.xz
+cd /home/lixq/src/nghttp2-${nghttp2ver} || exit 1
+[[ -d /home/lixq/toolchains/curl/libs/nghttp2 ]] || mkdir -p /home/lixq/toolchains/curl/libs/nghttp2
+./configure --prefix=/home/lixq/toolchains/curl/libs/nghttp2 --enable-lib-only || exit 1
+make || exit 1
+make install || exit 1
 
 cd /home/lixq/src || exit 1
 rm -rfv openssl
@@ -43,6 +53,6 @@ rm -rfv curl-${ver}
 tar -xf /home/lixq/35share-rd/src/curl-${ver}.tar.xz
 cd /home/lixq/src/curl-${ver} || exit 1
 autoreconf -fi || exit 1
-LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/libs/openssl/lib64" ./configure --prefix=/home/lixq/toolchains/curl --with-openssl=/home/lixq/toolchains/curl/libs/openssl --with-nghttp3=/home/lixq/toolchains/curl/libs/nghttp3 --with-ngtcp2=/home/lixq/toolchains/curl/libs/ngtcp2 || exit 1
+LDFLAGS="-Wl,-rpath,/home/lixq/toolchains/curl/libs/openssl/lib64" ./configure --prefix=/home/lixq/toolchains/curl --with-nghttp2=/home/lixq/toolchains/curl/libs/nghttp2 --with-openssl=/home/lixq/toolchains/curl/libs/openssl --with-nghttp3=/home/lixq/toolchains/curl/libs/nghttp3 --with-ngtcp2=/home/lixq/toolchains/curl/libs/ngtcp2 || exit 1
 make || exit 1
 make install || exit 1
