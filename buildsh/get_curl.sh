1e576ed8b908e1e66e192437de9da20571801f2b
lixq <lixq@wangsu.com>
2024-11-01 10:20:48 +0800

    curl http2

--- buildsh/get_curl.sh
+++ buildsh/get_curl.sh
@@ -3,6 +3,7 @@
 rm -rf openssl openssl.tar.xz
 git clone --depth 1 -b openssl-3.1.4+quic https://github.com/quictls/openssl
 tar -cJf openssl.tar.xz openssl
+rm -rf openssl
 
 rm -rf nghttp3 nghttp3.tar.xz
 git clone -b v1.1.0 https://github.com/ngtcp2/nghttp3
@@ -10,9 +11,12 @@ cd nghttp3 || exit 1
 git submodule update --init
 cd .. || exit 1
 tar -cJf nghttp3.tar.xz nghttp3
+rm -rf nghttp3
 
 rm -rf ngtcp2 ngtcp2.tar.xz
 git clone -b v1.2.0 https://github.com/ngtcp2/ngtcp2
 tar -cJf ngtcp2.tar.xz ngtcp2
+rm -rf ngtcp2
 
+wget https://github.com/nghttp2/nghttp2/releases/download/v1.64.0/nghttp2-1.64.0.tar.xz
 wget https://github.com/curl/curl/releases/download/curl-8_10_1/curl-8.10.1.tar.xz
