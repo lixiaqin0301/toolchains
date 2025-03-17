93d004dbbcbaea9b22b2fb3161bedcf22a2b6fb0
LiXiaqin <lixiaqin0301@gmail.com>
2025-03-17 22:17:32 +0800

    llvm asan

--- buildsh/build_llvm.sh
+++ buildsh/build_llvm.sh
@@ -14,6 +14,14 @@ if [[ ! -f /home/lixq/35share-rd/src/clang-${ver}.src.tar.xz ]]; then
     echo "wget https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/LLVM%20${ver}/clang-${ver}.src.tar.xz"
     need_exit=yes
 fi
+if [[ ! -f /home/lixq/35share-rd/src/compiler-rt-${ver}.src.tar.xz ]]; then
+    echo "wget https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/LLVM%20${ver}/compiler-rt-${ver}.src.tar.xz"
+    need_exit=yes
+fi
+if [[ ! -f /home/lixq/35share-rd/src/runtimes-${ver}.src.tar.xz ]]; then
+    echo "wget https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/LLVM%20${ver}/runtimes-${ver}.src.tar.xz"
+    need_exit=yes
+fi
 if [[ ! -f  /home/lixq/35share-rd/src/cmake-${ver}.src.tar.xz ]]; then
     echo "wget https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/LLVM%20${ver}/cmake-${ver}.src.tar.xz"
     need_exit=yes
@@ -34,6 +42,10 @@ tar -xf /home/lixq/35share-rd/src/llvm-${ver}.src.tar.xz
 mv llvm-${ver}.src llvm
 tar -xf /home/lixq/35share-rd/src/clang-${ver}.src.tar.xz
 mv clang-${ver}.src clang
+tar -xf /home/lixq/35share-rd/src/compiler-rt-${ver}.src.tar.xz
+mv compiler-rt-${ver}.src compiler-rt
+tar -xf /home/lixq/35share-rd/src/runtimes-${ver}.src.tar.xz
+mv runtimes-${ver}.src runtimes
 tar -xf /home/lixq/35share-rd/src/cmake-${ver}.src.tar.xz
 mv cmake-${ver}.src cmake
 tar -xf /home/lixq/35share-rd/src/third-party-${ver}.src.tar.xz
@@ -42,7 +54,7 @@ mv third-party-${ver}.src third-party
 mkdir /home/lixq/src/llvm-project/build
 cd /home/lixq/src/llvm-project/build || exit 1
 
-cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/llvm-${ver} -G "Unix Makefiles" ../llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_ENABLE_RUNTIMES="compiler-rt" -DCMAKE_INSTALL_PREFIX=/home/lixq/toolchains/llvm-${ver} -G "Unix Makefiles" ../llvm
 make || exit 1
 rm -rf /home/lixq/toolchains/llvm-${ver}
 make install || exit 1
