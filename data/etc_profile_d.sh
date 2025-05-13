a464e5e317fc780f1bb2125d9d50b5446ae4a130
lixq <lixq@wangsu.com>
2025-05-13 13:40:43 +0800

    del data

--- data/etc_profile_d.sh
+++
@@ -1,9 +0,0 @@
-export XIM=fcitx
-export XIM_PROGRAM=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS="@im=fcitx"
-#export ASAN_OPTIONS=log_path=/tmp/asan.log:detect_leaks=false:abort_on_error=true:disable_coredump=false:detect_odr_violation=0
-export ASAN_OPTIONS=log_path=/tmp/asan.log:detect_leaks=false:detect_odr_violation=0
-export GOPROXY=https://repo.haplat.net/go
-export NODE_EXTRA_CA_CERTS="/home/lixq/toolchains/WangSu_Root_CA.pem"
