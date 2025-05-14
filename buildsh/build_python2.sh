9e897547e06e0423fe064ae8b313d0d0f53066e6
lixq <lixq@wangsu.com>
2025-05-14 17:37:04 +0800

    python2

---
+++ buildsh/build_python2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ver=2.7.18
+
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+
+if [[ ! -f /home/lixq/35share-rd/src/Python-${ver}.tar.xz ]]; then
+    echo "wget https://www.python.org/ftp/python/${ver}/Python-${ver}.tar.xz"
+    exit 1
+fi
+[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
+cd /home/lixq/src || exit 1
+rm -rf Python-${ver}
+tar -xf /home/lixq/35share-rd/src/Python-${ver}.tar.xz
+cd Python-${ver} || exit 1
+./configure --prefix=/home/lixq/toolchains/Python-${ver} --with-ensurepip=install || exit 1
+make || exit 1
+rm -rf /home/lixq/toolchains/Python-${ver}
+make install || exit 1
+if [[ -d /home/lixq/toolchains/Python-${ver} ]]; then
+    cd /home/lixq/toolchains || exit 1
+    rm -f Python
+    ln -s Python-${ver} Python
+fi
