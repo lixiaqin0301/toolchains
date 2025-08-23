3435ea00dfaa91abf71b7efec500bcdee1fa6f90
LiXiaqin <lixiaqin0301@gmail.com>
2025-08-23 11:30:12 +0800

    all

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -1,75 +1,58 @@
 #!/bin/bash
+
+function build_packages() {
+    n=$1
+    shift
+    pkgs=("$@")
+    tsb=$(date +%s)
+    date "+%Y-%m-%d %H:%M:%S begin step $n ${pkgs[*]}" >> /tmp/build_all.log
+    for p in "${pkgs[@]}"; do
+        [[ -d /home/lixq/toolchains/$p ]] && continue
+        date "+%Y-%m-%d %H:%M:%S begin build toolchains $p" >> /tmp/build_all.log
+        tb=$(date +%s)
+        "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
+        if grep -q 'set_build_env.sh.*{name}' "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh"; then
+            "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
+        fi
+        te=$(date +%s)
+        date "+%Y-%m-%d %H:%M:%S end   build toolchains $p use $((te - tb)) seconds" >> /tmp/build_all.log
+    done
+    for td in toolset mintoolset; do
+        [[ -f /home/lixq/$td.tar.$n ]] && continue
+        rm -rf /home/lixq/$td
+        cd /home/lixq || exit 1
+        [[ -s $td.tar.$((n-1)) ]] && tar -xf $td.tar.$((n-1))
+        for p in "${pkgs[@]}"; do
+            date "+%Y-%m-%d %H:%M:%S begin build $td $p" >> /tmp/build_all.log
+            tb=$(date +%s)
+            "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" /home/lixq/$td || exit 1
+            te=$(date +%s)
+            date "+%Y-%m-%d %H:%M:%S end   build $td $p use $((te - tb)) seconds" >> /tmp/build_all.log
+        done
+        cd /home/lixq || exit 1
+        tar -cf $td.tar.$n $td
+    done
+    tse=$(date +%s)
+    date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >> /tmp
+}
+
 date "+%Y-%m-%d %H:%M:%S begin" > /tmp/build_all.log
 tab=$(date +%s)
 
 # step 1 gcc
-# binutils  2.45    https://mirrors.tuna.tsinghua.edu.cn/gnu/binutils/
 # gcc       15.2.0  https://mirrors.tuna.tsinghua.edu.cn/gnu/gcc/
+# binutils  2.45    https://mirrors.tuna.tsinghua.edu.cn/gnu/binutils/
 # ./contrib/download_prerequisites https://gcc.gnu.org/pub/gcc/infrastructure/
 n=1
-pkgs=(binutils gcc)
-tsb=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S begin step $n ${pkgs[*]}" >> /tmp/build_all.log
-for p in "${pkgs[@]}"; do
-    [[ -d /home/lixq/toolchains/$p ]] && continue
-    date "+%Y-%m-%d %H:%M:%S begin build toolchains $p" >> /tmp/build_all.log
-    tb=$(date +%s)
-    "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
-    te=$(date +%s)
-    date "+%Y-%m-%d %H:%M:%S end   build toolchains $p use $((te - tb)) seconds" >> /tmp/build_all.log
-done
 touch /home/lixq/mintoolset.tar.$n
-for td in toolset mintoolset; do
-    [[ -f /home/lixq/$td.tar.$n ]] && continue
-    rm -rf /home/lixq/$td
-    cd /home/lixq || exit 1
-    [[ -s $td.tar.$((n-1)) ]] && tar -xf $td.tar.$((n-1))
-    for p in "${pkgs[@]}"; do
-        date "+%Y-%m-%d %H:%M:%S begin build $td $p" >> /tmp/build_all.log
-        tb=$(date +%s)
-        "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" /home/lixq/$td || exit 1
-        te=$(date +%s)
-        date "+%Y-%m-%d %H:%M:%S end   build $td $p use $((te - tb)) seconds" >> /tmp/build_all.log
-    done
-    cd /home/lixq || exit 1
-    tar -cf $td.tar.$n $td
-done
-tse=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >> /tmp/build_all.log
+build_packages $n gcc binutils
 
-# step 2 cmake
+# step 2 cmake Bear
 # cmake  4.1.0  https://cmake.org/download/
 # Bear   3.1.6  https://github.com/rizsotto/Bear/
 n=2
-pkgs=(cmake Bear)
-tsb=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S begin step $n ${pkgs[*]}" >> /tmp/build_all.log
-for p in "${pkgs[@]}"; do
-    [[ -d /home/lixq/toolchains/$p ]] && continue
-    date "+%Y-%m-%d %H:%M:%S begin build toolchains $p" >> /tmp/build_all.log
-    tb=$(date +%s)
-    "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
-    te=$(date +%s)
-    date "+%Y-%m-%d %H:%M:%S end   build toolchains $p use $((te - tb)) seconds" >> /tmp/build_all.log
-done
 [[ -f /home/lixq/mintoolset.tar.$n ]] || cp /home/lixq/mintoolset.tar.$((n-1)) /home/lixq/mintoolset.tar.$n
-for td in toolset mintoolset; do
-    [[ -f /home/lixq/$td.tar.$n ]] && continue
-    rm -rf /home/lixq/$td
-    cd /home/lixq || exit 1
-    [[ -s $td.tar.$((n-1)) ]] && tar -xf $td.tar.$((n-1))
-    for p in "${pkgs[@]}"; do
-        date "+%Y-%m-%d %H:%M:%S begin build $td $p" >> /tmp/build_all.log
-        tb=$(date +%s)
-        "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" /home/lixq/$td || exit 1
-        te=$(date +%s)
-        date "+%Y-%m-%d %H:%M:%S end   build $td $p use $((te - tb)) seconds" >> /tmp/build_all.log
-    done
-    cd /home/lixq || exit 1
-    tar -cf $td.tar.$n $td
-done
-tse=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >> /tmp/build_all.log
+build_packages $n cmake Bear
 
 # step 3 curl
 # openssl       3.5.2   https://github.com/openssl/openssl/releases/
@@ -88,70 +71,15 @@ date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >>
 # libpsl        0.21.5  https://github.com/rockdaboot/libpsl/releases/
 # gsasl         2.2.2   https://mirrors.tuna.tsinghua.edu.cn/gnu/gsasl/
 # curl          8.15.0  https://github.com/curl/curl/releases/
-n=3
-pkgs=(openssl nghttp3 ngtcp2 nghttp2 libssh2 zlib brotli zstd keyutils krb5 libidn2 openldap libunistring libpsl gsasl curl)
-tsb=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S begin step $n ${pkgs[*]}" >> /tmp/build_all.log
-for p in "${pkgs[@]}"; do
-    [[ -d /home/lixq/toolchains/$p ]] && continue
-    date "+%Y-%m-%d %H:%M:%S begin build toolchains $p" >> /tmp/build_all.log
-    tb=$(date +%s)
-    "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
-    "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
-    te=$(date +%s)
-    date "+%Y-%m-%d %H:%M:%S end   build toolchains $p use $((te - tb)) seconds" >> /tmp/build_all.log
-done
-for td in toolset mintoolset; do
-    [[ -f /home/lixq/$td.tar.$n ]] && continue
-    rm -rf /home/lixq/$td
-    cd /home/lixq || exit 1
-    [[ -s $td.tar.$((n-1)) ]] && tar -xf $td.tar.$((n-1))
-    for p in "${pkgs[@]}"; do
-        date "+%Y-%m-%d %H:%M:%S begin build $td $p" >> /tmp/build_all.log
-        tb=$(date +%s)
-        "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" /home/lixq/$td || exit 1
-        te=$(date +%s)
-        date "+%Y-%m-%d %H:%M:%S end   build $td $p use $((te - tb)) seconds" >> /tmp/build_all.log
-    done
-    cd /home/lixq || exit 1
-    tar -cf $td.tar.$n $td
-done
-tse=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >> /tmp/build_all.log
+build_packages 3 nghttp3 ngtcp2 nghttp2 libssh2 zlib brotli zstd keyutils krb5 libidn2 openldap libunistring libpsl gsasl curl
 
-# step 4 bashdb bat
+# step 4 bashdb bat gdb
 # bashdb 4.4-1.0.1 https://sourceforge.net/projects/bashdb/files/bashdb/
 # bat    0.25.0    https://github.com/sharkdp/bat/releases/
+# gdb    16.3      https://mirrors.tuna.tsinghua.edu.cn/gnu/gdb/
 n=4
-pkgs=(bashdb bat)
-tsb=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S begin step $n ${pkgs[*]}" >> /tmp/build_all.log
-for p in "${pkgs[@]}"; do
-    [[ -d /home/lixq/toolchains/$p ]] && continue
-    date "+%Y-%m-%d %H:%M:%S begin build toolchains $p" >> /tmp/build_all.log
-    tb=$(date +%s)
-    "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" || exit 1
-    te=$(date +%s)
-    date "+%Y-%m-%d %H:%M:%S end   build toolchains $p use $((te - tb)) seconds" >> /tmp/build_all.log
-done
 [[ -f /home/lixq/mintoolset.tar.$n ]] || cp /home/lixq/mintoolset.tar.$((n-1)) /home/lixq/mintoolset.tar.$n
-for td in toolset mintoolset; do
-    [[ -f /home/lixq/$td.tar.$n ]] && continue
-    rm -rf /home/lixq/$td
-    cd /home/lixq || exit 1
-    [[ -s $td.tar.$((n-1)) ]] && tar -xf $td.tar.$((n-1))
-    for p in "${pkgs[@]}"; do
-        date "+%Y-%m-%d %H:%M:%S begin build $td $p" >> /tmp/build_all.log
-        tb=$(date +%s)
-        "/home/lixq/35share-rd/toolchains/buildsh/build_$p.sh" /home/lixq/$td || exit 1
-        te=$(date +%s)
-        date "+%Y-%m-%d %H:%M:%S end   build $td $p use $((te - tb)) seconds" >> /tmp/build_all.log
-    done
-    cd /home/lixq || exit 1
-    tar -cf $td.tar.$n $td
-done
-tse=$(date +%s)
-date "+%Y-%m-%d %H:%M:%S end   step $n ${pkgs[*]} use $((tse - tsb)) seconds" >> /tmp/build_all.log
+build_packages $n bashdb bat gdb
 
 tae=$(date +%s)
 date "+%Y-%m-%d %H:%M:%S end   use $((tae - tab)) seconds" >> /tmp/build_all.log
--- buildsh/build_gdb.sh
+++ buildsh/build_gdb.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
+name=gdb
 ver=16.3
+srcpath=/home/lixq/src/${name}-${ver}.tar.gz
+DESTDIR=/home/lixq/toolchains/${name}
+[[ -n "$1" ]] && DESTDIR="$1"
 
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
-. /opt/rh/devtoolset-11/enable
-
-if [[ ! -f /home/lixq/src/gdb-${ver}.tar.xz ]]; then
-    echo "wget https://mirrors.tuna.tsinghua.edu.cn/gnu/gdb/gdb-${ver}.tar.xz"
-    exit 1
+if [[ "$DESTDIR" == */${name} ]]; then
+    . "$(dirname "${BASH_SOURCE[0]}")/set_build_env.sh" gcc
+else
+    . "$(dirname "${BASH_SOURCE[0]}")/set_build_env.sh" "$(basename "$DESTDIR")"
 fi
+
 [[ -d /home/lixq/src ]] || mkdir /home/lixq/src
 cd /home/lixq/src || exit 1
-rm -rf gdb-${ver}
-tar -xvf /home/lixq/src/gdb-${ver}.tar.xz
-mkdir gdb-${ver}/build
-cd gdb-${ver}/build || exit 1
-../configure --prefix=/home/lixq/toolchains/gdb-${ver} --with-lzma || exit 1
-make || exit 1
-rm -rf /home/lixq/toolchains/gdb-${ver}
-make install || exit 1
-if [[ -d /home/lixq/toolchains/gdb-${ver} ]]; then
-    cd /home/lixq/toolchains || exit 1
-    rm -f gdb
-    ln -s gdb-${ver} gdb
-fi
+rm -rf ${name}-${ver}
+tar -xf /home/lixq/src/${name}-${ver}.tar.gz || exit 1
+mkdir ${name}-${ver}/build
+cd ${name}-${ver}/build || exit 1
+../configure --prefix="$DESTDIR/usr" || exit 1
+make -s -j"$(nproc)" || exit 1
+[[ "$DESTDIR" == */${name} ]] && rm -rf "$DESTDIR"
+make -s -j"$(nproc)" install || exit 1
