0b6bb839a6a67261b6732b7a36c0c25680a06dad
lixq <lixq@lixq.wangsu.com>
2025-08-26 18:21:46 +0800

    selinux

--- buildsh/build_selinux.sh
+++ buildsh/build_selinux.sh
@@ -13,6 +13,7 @@ cd /home/lixq/src || exit 1
 rm -rf ${name}-${ver}
 tar -xf $srcpath || exit 1
 cd ${name}-${ver} || exit 1
-make -s -j"$(nproc)" || exit 1
+sed -i 's;sepol/cil/cil.h;../../libsepol/cil/include/cil/cil.h;' libsemanage/src/semanage_store.h libsemanage/src/direct_api.c
+make || exit 1
 [[ "$DESTDIR" == */${name} ]] && rm -rf "$DESTDIR"
 make -s -j"$(nproc)" install DESTDIR="$DESTDIR" || exit 1
