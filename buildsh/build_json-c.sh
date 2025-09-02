ad9d0299fa753f9b388e754714f01dccab891085
lixq <lixq@lixq.wangsu.com>
2025-09-02 18:11:38 +0800

    bcc

--- buildsh/build_json-c.sh
+++ buildsh/build_json-c.sh
@@ -18,6 +18,6 @@ tar -xf "$srcpath" || exit 1
 cd "json-c-$name-$ver" || exit 1
 mkdir json-c-build
 cd json-c-build || exit 1
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX="$DESTDIR/usr" .. || exit 1
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_INSTALL_PREFIX="$DESTDIR/usr" -DCMAKE_INSTALL_LIBDIR=lib .. || exit 1
 make -s "-j$(nproc)" || exit 1
 make -s "-j$(nproc)" install || exit 1
