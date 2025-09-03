13a9021c52e1d775b7d2d143659dfbd84b3eba07
lixq <lixq@lixq.wangsu.com>
2025-09-03 15:30:17 +0800

    systemtap

--- buildsh/build_glibc.sh
+++ buildsh/build_glibc.sh
@@ -11,8 +11,8 @@ kernelver=6.6
 [[ -f $srcpath ]] || exit 1
 [[ -f /home/lixq/src/linux-$kernelver.tar.gz ]] || exit 1
 
-export PATH="/home/lixq/toolchains/patchelf/usr/bin:/home/lixq/toolchains/make/usr/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export LDFLAGS="-static-libgcc -static-libstdc++"
+export PATH="$DESTDIR/usr/bin:/home/lixq/toolchains/patchelf/usr/bin:/home/lixq/toolchains/make/usr/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export LD_RUN_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib"
 
 [[ -d /home/lixq/src ]] || mkdir /home/lixq/src
 cd /home/lixq/src || exit 1
@@ -26,16 +26,17 @@ make -s "-j$(nproc)" install "DESTDIR=$DESTDIR" || exit 1
 if [[ $DESTDIR == */$name ]]; then
     make -s "-j$(nproc)" localedata/install-locales "DESTDIR=$DESTDIR" || exit 1
     make -s "-j$(nproc)" localedata/install-locale-files "DESTDIR=$DESTDIR" || exit 1
-else
-    cd "$DESTDIR/lib64" || exit 1
-    cp /home/lixq/toolchains/gcc/usr/lib64/libgcc* .
-    for p in /home/lixq/toolchains/gcc/usr/lib64/libstdc++.s*[0-9o]; do
-        if [[ -L "$p" ]]; then
-            ln -sf "$(readlink "$p")" "$(basename "$p")"
-        else
-            cp "$p" .
-        fi
-    done
+# else
+#     cd "$DESTDIR/lib64" || exit 1
+#     cp /home/lixq/toolchains/gcc/usr/lib64/libgcc* .
+#     for p in /home/lixq/toolchains/gcc/usr/lib64/libstdc++.s*[0-9o]; do
+#         [[ -f $(basename "$p") ]] && continue
+#         if [[ -L $p ]]; then
+#             ln -sf "$(readlink "$p")" "$(basename "$p")"
+#         else
+#             cp "$p" .
+#         fi
+#     done
 fi
 
 cd /home/lixq/src || exit 1
@@ -44,10 +45,10 @@ tar -xf /home/lixq/src/linux-$kernelver.tar.gz || exit 1
 cd linux-${kernelver} || exit 1
 make -s "-j$(nproc)" headers_install "INSTALL_HDR_PATH=$DESTDIR/usr" || exit 1
 
-for f in "$DESTDIR"/usr/*bin/* "$DESTDIR"/*bin/*; do
-    [[ -L $f ]] && continue
-    ldd "$f" 2>&1 | grep -q ': version .GLIBC_.* not found' || continue
-    patchelf --set-rpath "$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib" "$f"
-    file "$f" | grep -q 'uses shared libs' || continue
-    patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
-done
+# for f in "$DESTDIR"/usr/*bin/* "$DESTDIR"/*bin/*; do
+#     [[ -L $f ]] && continue
+#     ldd "$f" 2>&1 | grep -q ': version .GLIBC_.* not found' || continue
+#     patchelf --set-rpath "$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib" "$f"
+#     file "$f" | grep -q 'uses shared libs' || continue
+#     patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
+# done
--- buildsh/build_systemtap.sh
+++ buildsh/build_systemtap.sh
@@ -9,7 +9,7 @@ srcpath=/home/lixq/src/$name-$ver.tar.gz
 [[ -n $DESTDIR ]] || exit 1
 [[ -f $srcpath ]] || exit 1
 
-export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PATH="$DESTDIR/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export CPATH="/home/lixq/toolchains/boost_1_88_0/usr/include"
 export LIBRARY_PATH="/home/lixq/toolchains/boost_1_88_0/usr/lib"
 export LDFLAGS="-L$LIBRARY_PATH -Wl,-rpath-link,$LIBRARY_PATH"
@@ -26,19 +26,19 @@ make -s "-j$(nproc)" install || exit 1
 
 [[ -d $DESTDIR/usr/lib ]] || mkdir -p "$DESTDIR/usr/lib"
 [[ -f $DESTDIR/usr/lib/libboost_system.so.1.88.0 ]] || cp /home/lixq/toolchains/boost_1_88_0/usr/lib/libboost_system.so.1.88.0 "$DESTDIR/usr/lib/libboost_system.so.1.88.0"
-[[ -d $DESTDIR/lib64 ]] || mkdir -p "$DESTDIR/lib64"
-for f in "$DESTDIR/usr/bin/"* "$DESTDIR/usr/libexec/systemtap/stap"*; do
-    ldd "$f" | grep ' => /[^h]' | awk '{print $3}' | while read -r lib; do
-        if [[ ! -f $DESTDIR$lib ]]; then
-            [[ -d $(dirname "$DESTDIR$lib") ]] || mkdir -p "$(dirname "$DESTDIR$lib")"
-            if [[ -f /home/lixq/toolchains/glibc$lib ]]; then
-                cp "/home/lixq/toolchains/glibc$lib" "$DESTDIR$lib"
-            else
-                cp "$lib" "$DESTDIR$lib"
-            fi
-        fi
-        patchelf --set-rpath "$LD_RUN_PATH" "$DESTDIR$lib"
-    done
-    patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
-done
-cp /home/lixq/toolchains/glibc/lib64/ld-linux-x86-64.so.2 "$DESTDIR/lib64"
+# [[ -d $DESTDIR/lib64 ]] || mkdir -p "$DESTDIR/lib64"
+# for f in "$DESTDIR/usr/bin/"* "$DESTDIR/usr/libexec/systemtap/stap"*; do
+#     ldd "$f" | grep ' => /[^h]' | awk '{print $3}' | while read -r lib; do
+#         if [[ ! -f $DESTDIR$lib ]]; then
+#             [[ -d $(dirname "$DESTDIR$lib") ]] || mkdir -p "$(dirname "$DESTDIR$lib")"
+#             if [[ -f /home/lixq/toolchains/glibc$lib ]]; then
+#                 cp "/home/lixq/toolchains/glibc$lib" "$DESTDIR$lib"
+#             else
+#                 cp "$lib" "$DESTDIR$lib"
+#             fi
+#         fi
+#         patchelf --set-rpath "$LD_RUN_PATH" "$DESTDIR$lib"
+#     done
+#     patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
+# done
+# cp /home/lixq/toolchains/glibc/lib64/ld-linux-x86-64.so.2 "$DESTDIR/lib64"
