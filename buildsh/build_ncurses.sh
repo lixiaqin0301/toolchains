8fbe17a78d8c0d0538b59d5683cbb2454b76c1b4
lixq <lixq@lixq.wangsu.com>
2025-09-04 16:39:23 +0800

    systemtap

--- buildsh/build_ncurses.sh
+++ buildsh/build_ncurses.sh
@@ -16,10 +16,22 @@ cd /home/lixq/src || exit 1
 rm -rf "$name-$ver"
 tar -xf "$srcpath" || exit 1
 cd "$name-$ver" || exit 1
-./configure "--prefix=$DESTDIR/usr" --with-shared --enable-widec --without-ada || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
-cd "$DESTDIR/usr/include/" || exit 1
-for f in ncursesw/*.h; do
-    ln -s "$f" .
+ncurses_options=(--with-shared --without-ada --with-ospeed=unsigned --enable-hard-tabs --enable-xmc-glitch --enable-colorfgbg --enable-overwrite --enable-pc-files --with-termlib=tinfo --with-chtype=long --with-cxx-shared --with-xterm-kbs=DEL)
+mkdir narrowc widec
+cd narrowc || exit 1
+ln -s ../configure .
+./configure "${ncurses_options[@]}" --with-ticlib || exit 1
+make -s "-j$(nproc)" libs || exit 1
+make -s "-j$(nproc)" -C progs || exit 1
+cd ../widec || exit 1
+ln -s ../configure .
+./configure "${ncurses_options[@]}" --enable-widec --without-progs || exit 1
+make -s "-j$(nproc)" libs || exit 1
+cd ..
+make -C narrowc "DESTDIR=$DESTDIR" install.{libs,progs,data}
+make -C widec "DESTDIR=$DESTDIR" install.{libs,includes,man}
+mkdir "$DESTDIR/usr/include"/ncurses{,w}
+for l in "$DESTDIR/usr/include"/*.h; do
+    ln -s "../$(basename "$l")" "$DESTDIR/usr/include/ncurses"
+    ln -s "../$(basename "$l")" "$DESTDIR/usr/include/ncursesw"
 done
