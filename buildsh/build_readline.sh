367fd6cf18c06c9193766e9f42d2ec59d35954c0
lixq <lixq@lixq.wangsu.com>
2025-09-04 10:37:10 +0800

    systemtap

---
+++ buildsh/build_readline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+name=$(basename "${BASH_SOURCE[0]}" .sh)
+name=${name#build_}
+ver=8.3
+DESTDIR=$1
+srcpath=/home/lixq/src/$name-$ver.tar.gz
+
+[[ -n $DESTDIR ]] || exit 1
+[[ -f $srcpath ]] || exit 1
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
+cd /home/lixq/src || exit 1
+rm -rf "$name-$ver"
+tar -xf "$srcpath" || exit 1
+cd "$name-$ver" || exit 1
+./configure "--prefix=$DESTDIR/usr" || exit 1
+make -s "-j$(nproc)" || exit 1
+make -s "-j$(nproc)" install || exit 1
