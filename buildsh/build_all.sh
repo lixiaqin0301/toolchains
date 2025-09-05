0d5e93fac480dabfe80c915b34cf5487240e4c5b
lixq <lixq@lixq.wangsu.com>
2025-09-05 15:36:59 +0800

    cmak

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -27,8 +27,8 @@ function build_packages() {
 tab=$(date +%s)
 date "+%Y-%m-%d %H:%M:%S begin" | tee /tmp/build_all.log
 
-# cmake  4.1.0  https://cmake.org/download/
-build_packages 4.1.0 /home/lixq/toolchains/cmake cmake
+# cmake  4.1.1  https://cmake.org/download/
+build_packages 4.1.1 /home/lixq/toolchains/cmake cmake
 
 # Shellcheck  0.11.0  https://github.com/koalaman/shellcheck/releases
 build_packages 0.11.0 /home/lixq/toolchains/shellcheck shellcheck
@@ -52,17 +52,19 @@ build_packages 4.4-1.0.1 /home/lixq/toolchains/bashdb bashdb
 # bat  0.25.0  https://github.com/sharkdp/bat/releases/
 build_packages 0.25.0 /home/lixq/toolchains/bat bat
 
-# make             4.4.1   https://mirrors.tuna.tsinghua.edu.cn/gnu/make/
-# patchelf         0.18.0  https://github.com/NixOS/patchelf/releases/
-# pcre2            10.45   https://github.com/PCRE2Project/pcre2/releases/
-# audit-userspace  4.1.1   https://github.com/linux-audit/audit-userspace/releases/
-# libcap           2.76    https://git.kernel.org/pub/scm/libs/libcap/libcap.git/snapshot/
-# glibc            2.42    https://mirrors.ustc.edu.cn/gnu/glibc/
-# libselinux       3.9     https://github.com/SELinuxProject/selinux/tags
+# make  4.4.1  https://mirrors.tuna.tsinghua.edu.cn/gnu/make/
 build_packages 4.4.1 /home/lixq/toolchains/make make
-build_packages 0.18.0 /home/lixq/toolchains/patchelf patchelf
-build_packages 2.42 /home/lixq/toolchains/glibc pcre2 audit-userspace libcap glibc libselinux
 
+# patchelf  0.18.0  https://github.com/NixOS/patchelf/releases/
+build_packages 0.18.0 /home/lixq/toolchains/patchelf patchelf
+
+# glibc            2.42    https://mirrors.ustc.edu.cn/gnu/glibc/
+# pcre2            10.46   https://github.com/PCRE2Project/pcre2/releases/
+# audit-userspace  4.1.2   https://github.com/linux-audit/audit-userspace/releases/
+# libcap           2.76    https://git.kernel.org/pub/scm/libs/libcap/libcap.git/
+build_packages 2.42 /home/lixq/toolchains/glibc pcre2 audit-userspace libcap glibc
+
+# curl          8.15.0  https://github.com/curl/curl/releases/
 # openssl       3.5.2   https://github.com/openssl/openssl/releases/
 # nghttp3       1.11.0  https://github.com/ngtcp2/nghttp3/releases/
 # ngtcp2        1.14.0  https://github.com/ngtcp2/ngtcp2/releases/
@@ -78,9 +80,9 @@ build_packages 2.42 /home/lixq/toolchains/glibc pcre2 audit-userspace libcap gli
 # libunistring  1.3     https://mirrors.tuna.tsinghua.edu.cn/gnu/libunistring/
 # libpsl        0.21.5  https://github.com/rockdaboot/libpsl/releases/
 # gsasl         2.2.2   https://mirrors.tuna.tsinghua.edu.cn/gnu/gsasl/
-# curl          8.15.0  https://github.com/curl/curl/releases/
 build_packages 8.15.0 /home/lixq/toolchains/curl keyutils libidn2 libunistring zlib zstd openssl nghttp3 ngtcp2 nghttp2 libssh2 brotli krb5 openldap libpsl gsasl curl
 
+# llvm     21.1.0        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
 # bison    3.8.2         https://mirrors.tuna.tsinghua.edu.cn/gnu/bison/
 # swig     4.3.1         https://github.com/swig/swig/tags
 # libedit  20250104-3.1  https://thrysoee.dk/editline/
@@ -89,7 +91,6 @@ build_packages 8.15.0 /home/lixq/toolchains/curl keyutils libidn2 libunistring z
 # libxml2  2.14.5        https://gitlab.gnome.org/GNOME/libxml2/-/releases
 # Python   3.13.7        https://www.python.org/ftp/python/
 # lua      5.4.8         https://www.lua.org/ftp/
-# llvm     21.1.0        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
 build_packages 21.1.0 /home/lixq/toolchains/llvm bison swig ncurses libedit xz zlib libxml2 openssl Python lua llvm
 
 # zsh  5.9  https://www.zsh.org/
@@ -101,8 +102,8 @@ build_packages 1.89.0 /home/lixq/toolchains/boost boost
 # tcpflow  1.6.1  https://github.com/simsong/tcpflow/releases/
 build_packages 1.6.1 /home/lixq/toolchains/tcpflow tcpflow
 
-# expat 2.7.1  https://github.com/libexpat/libexpat/releases/
 # git  2.51.0  https://github.com/git/git/tags
+# expat 2.7.1  https://github.com/libexpat/libexpat/releases/
 build_packages 2.51.0 /home/lixq/toolchains/git expat libpsl zlib openssl curl glibc git
 
 # node  24.7.0  https://nodejs.org/dist
@@ -111,6 +112,7 @@ build_packages 24.7.0 /home/lixq/toolchains/node glibc node
 # bpftrace  0.23.5 https://github.com/bpftrace/bpftrace/releases
 build_packages  0.23.5  /home/lixq/toolchains/bpftrace bpftrace
 
+# bcc       0.35.0         https://github.com/iovisor/bcc/releases/
 # bzip2     1.0.8          https://sourceware.org/pub/bzip2/
 # elfutils  0.193          https://sourceware.org/elfutils/ftp/
 # flex      2.6.4          https://github.com/westes/flex/releases/
@@ -119,47 +121,11 @@ build_packages  0.23.5  /home/lixq/toolchains/bpftrace bpftrace
 # LuaJIT    2.1.ROLLING    https://github.com/LuaJIT/LuaJIT/tags
 # netperf   2.7.0          https://github.com/HewlettPackard/netperf/tags
 # libbpf    1.6.2          https://github.com/libbpf/libbpf/releases
-# bcc       0.35.0         https://github.com/iovisor/bcc/releases/
 build_packages 0.35.0 /home/lixq/toolchains/bcc bison brotli bzip2 flex icu4c json-c libbpf libedit libffi libpsl libxml2 LuaJIT ncurses netperf openssl xz zlib zstd curl elfutils glibc Python bcc
 
 # systemtap  5.3  https://sourceware.org/systemtap/ftp/releases/
 # readline   8.3  https://mirrors.tuna.tsinghua.edu.cn/gnu/readline/
 build_packages 5.3 /home/lixq/toolchains/systemtap binutils bzip2 curl elfutils gcc_10.4.0 ncurses patchelf readline xz zlib zstd systemtap glibc
-# DESTDIR=/home/lixq/toolchains/systemtap
-# patchbins=("$DESTDIR/usr/bin/stap" "$DESTDIR/usr/bin/stapbpf" "$DESTDIR/usr/bin/stap-merge" "$DESTDIR/usr/bin/staprun" "$DESTDIR/usr/bin/stapsh" "$DESTDIR/usr/libexec/systemtap/stapio")
-# [[ -d $DESTDIR/lib64 ]] || mkdir -p "$DESTDIR/lib64"
-# for f in "$DESTDIR/usr/bin/"* "$DESTDIR/usr/libexec/systemtap/stap"*; do
-#     ldd "$f" | grep ' => /[^h]' | awk '{print $3}' | while read -r lib; do
-#         if [[ ! -f $DESTDIR$lib ]]; then
-#             [[ -d $(dirname "$DESTDIR$lib") ]] || mkdir -p "$(dirname "$DESTDIR$lib")"
-#             if [[ -f /home/lixq/toolchains/glibc$lib ]]; then
-#                 cp "/home/lixq/toolchains/glibc$lib" "$DESTDIR$lib"
-#             else
-#                 cp "$lib" "$DESTDIR$lib"
-#             fi
-#         fi
-#         patchelf --set-rpath "$LD_RUN_PATH" "$DESTDIR$lib"
-#     done
-#     patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
-# done
-# cp /home/lixq/toolchains/glibc/lib64/ld-linux-x86-64.so.2 "$DESTDIR/lib64"
-# for f in "$DESTDIR"/usr/*bin/* "$DESTDIR"/*bin/* "$DESTDIR"/usr/lib*/* "$DESTDIR"/lib*/* "$DESTDIR/usr/libexec/"*/*; do
-#     [[ -L $f ]] && continue
-#     [[ -d $f ]] && continue
-#     ldd "$f" 2>/dev/null | grep -q ' => ' || continue
-#     readelf -d "$f" | grep -q "$DESTDIR" || continue
-#     echo "$f"
-# done
 
-# while IFS= read -r f; do
-#     [[ -f "$DESTDIR$f" ]] || cp "$f" "$DESTDIR$f"
-# done < <(ldd "${patchbins[@]}" 2>&1 | grep ' => /[^h]' | awk '{print $3}' | sort -u)
-
-# while IFS= read -r f; do
-#     patchelf --set-rpath "$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib" "$f"
-# done < <(ldd "${patchbins[@]}" 2>&1 | grep ' => /h' | awk '{print $3}' | sort -u)
-    #patchelf --set-rpath "$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib" "$f"
-    #file "$f" | grep 'uses shared libs' || continue
-    #patchelf --set-interpreter "$DESTDIR/lib64/ld-linux-x86-64.so.2" "$f"
 tae=$(date +%s)
 date "+%Y-%m-%d %H:%M:%S end   use $((tae - tab)) seconds" | tee -a /tmp/build_all.log
--- buildsh/build_cmake.sh
+++ buildsh/build_cmake.sh
@@ -2,7 +2,7 @@
 
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=4.1.0
+ver=4.1.1
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver-linux-x86_64.tar.gz
 
@@ -11,7 +11,6 @@ srcpath=/home/lixq/src/$name-$ver-linux-x86_64.tar.gz
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-[[ $DESTDIR == */$name ]] && rm -rf "$DESTDIR"
 [[ -d $DESTDIR/usr ]] || mkdir -p "$DESTDIR/usr"
 cd "$DESTDIR/usr" || exit 1
 tar -xf "$srcpath" --strip-components=1 || exit 1
