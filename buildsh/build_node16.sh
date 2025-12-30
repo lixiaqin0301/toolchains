458a73788355ac476c03dfaae04ab98a7b6c324d
lixq <lixq@lixq.wangsu.com>
2025-12-30 18:53:16 +0800

    node 14.17.3

--- buildsh/build_node16.sh
+++ buildsh/build_node16.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ver=v16.20.2
+ver=v14.17.3
 
-. "$(dirname "${BASH_SOURCE[0]}")/set_build_env.sh" /home/lixq/toolchains/gcc /home/lixq/toolchains/binutils
-export PATH="$PATH:/home/lixq/toolchains/Bear/bin"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+. /opt/rh/devtoolset-11/enable
+export PATH="$PATH:/home/lixq/toolchains/Bear/usr/bin"
 export CFLAGS="-g $CFLAGS"
 export CXXFLAGS="-g $CXXFLAGS"
+
 if [[ ! -f /home/lixq/src/node-${ver}.tar.gz ]]; then
     echo "wget https://nodejs.org/dist/${ver}/node-${ver}.tar.gz"
     exit 1
@@ -15,7 +17,6 @@ cd /home/lixq/workspace-vscode || exit 1
 rm -rf node-${ver}
 tar -xf /home/lixq/src/node-${ver}.tar.gz
 cd /home/lixq/workspace-vscode/node-${ver} || exit 1
-sed -i '/#include <unordered_set>/a #include <cstdint>' src/inspector/worker_inspector.h
 ./configure --prefix=/home/lixq/toolchains/node16-${ver} --shared || exit 1
 bear -- make || exit 1
 rm -rf /home/lixq/toolchains/node16-${ver}
