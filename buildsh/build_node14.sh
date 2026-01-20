097ed02805902c9e33e637960e91608dbf63f06a
lixq <lixq@lixq.wangsu.com>
2026-01-20 13:40:42 +0800

    node14

--- buildsh/build_node14.sh
+++ buildsh/build_node14.sh
@@ -5,8 +5,8 @@ ver=v14.17.3
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 . /opt/rh/devtoolset-11/enable
 export PATH="$PATH:/home/lixq/toolchains/Bear/usr/bin"
-export CFLAGS="-g $CFLAGS"
-export CXXFLAGS="-g $CXXFLAGS"
+export CFLAGS="-g -fno-omit-frame-pointer $CFLAGS"
+export CXXFLAGS="-g -fno-omit-frame-pointer $CXXFLAGS"
 
 if [[ ! -f /home/lixq/src/node-${ver}.tar.gz ]]; then
     echo "wget https://nodejs.org/dist/${ver}/node-${ver}.tar.gz"
@@ -17,10 +17,10 @@ cd /home/lixq/workspace-vscode || exit 1
 rm -rf node-${ver}
 tar -xf /home/lixq/src/node-${ver}.tar.gz
 cd /home/lixq/workspace-vscode/node-${ver} || exit 1
-./configure --prefix=/home/lixq/toolchains/node16-${ver} --shared || exit 1
+./configure --prefix=/home/lixq/toolchains/node14-${ver} --shared --debug --debug-node --debug-lib --gdb --v8-non-optimized-debug --v8-with-dchecks --v8-enable-object-print || exit 1
 bear -- make || exit 1
-rm -rf /home/lixq/toolchains/node16-${ver}
+rm -rf /home/lixq/toolchains/node14-${ver}
 make install || exit 1
-cd /home/lixq/toolchains/node16-${ver}/lib || exit 1
+cd /home/lixq/toolchains/node14-${ver}/lib || exit 1
 ln -s libnode.so.* libnode.so
-cp /home/lixq/workspace-vscode/node-${ver}/deps/v8/include/v8-inspector*.h /home/lixq/toolchains/node16-${ver}/include/node/
+cp /home/lixq/workspace-vscode/node-${ver}/deps/v8/include/v8-inspector*.h /home/lixq/toolchains/node14-${ver}/include/node/
