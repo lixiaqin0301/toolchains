b55d48c419981501a59d56c788bebac5fcbb7e2c
lixq <lixq@lixq.wangsu.com>
2026-03-13 18:45:42 +0800

    nghttp3

--- buildsh/build_nghttp3.sh
+++ buildsh/build_nghttp3.sh
@@ -8,6 +8,7 @@ srcpath=/home/lixq/src/$name-$ver.tar.gz
 
 [[ -n $DESTDIR ]] || exit 1
 [[ -f $srcpath ]] || exit 1
+[[ -f /home/lixq/src/sfparse.tar.gz ]] || exit 1
 
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export PKG_CONFIG_PATH="$DESTDIR/usr/lib/pkgconfig"
@@ -17,7 +18,9 @@ export LIBRARY_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr
 cd /home/lixq/src || exit 1
 rm -rf "$name-$ver"
 tar -xf "$srcpath" || exit 1
-cd "$name-$ver" || exit 1
+cd "/home/lixq/src/$name-$ver/lib" || exit 1
+tar -xf /home/lixq/src/sfparse.tar.gz || exit 1
+cd "/home/lixq/src/$name-$ver" || exit 1
 autoreconf -fi || exit 1
 ./configure "--prefix=$DESTDIR/usr" || exit 1
 make -s "-j$(nproc)" || exit 1
