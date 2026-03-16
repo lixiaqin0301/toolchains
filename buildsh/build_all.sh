f64844065593b1d9a25036a0c9ba7e38ecf8e363
lixq <lixq@lixq.wangsu.com>
2026-03-16 14:28:08 +0800

    bpftrace llvm node patchelf

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -70,8 +70,8 @@ build_packages 0.26.1 /home/lixq/toolchains/bat bat
 # make  4.4.1  https://mirrors.tuna.tsinghua.edu.cn/gnu/make/
 build_packages 4.4.1 /home/lixq/toolchains/make make
 
-# patchelf  0.18.0  https://github.com/NixOS/patchelf/releases/
-build_packages 0.18.0 /home/lixq/toolchains/patchelf patchelf
+# patchelf  0.15.5  https://github.com/NixOS/patchelf/releases/
+build_packages 0.15.5 /home/lixq/toolchains/patchelf patchelf
 
 # glibc            2.43     https://mirrors.ustc.edu.cn/gnu/glibc/
 # kernel           6.6.121  https://www.kernel.org/
@@ -99,7 +99,7 @@ build_packages 2.43 /home/lixq/toolchains/glibc pcre2 audit-userspace libcap gli
 # gsasl         2.2.2   https://mirrors.tuna.tsinghua.edu.cn/gnu/gsasl/
 build_packages 8.19.0 /home/lixq/toolchains/curl brotli c-ares gsasl keyutils krb5 libidn2 libpsl libunistring zlib zstd openssl nghttp3 ngtcp2 nghttp2 libssh2 openldap curl
 
-# llvm     22.1.0        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
+# llvm     22.1.1        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
 # bison    3.8.2         https://mirrors.tuna.tsinghua.edu.cn/gnu/bison/
 # libedit  20251016-3.1  https://thrysoee.dk/editline/
 # libxml2  2.15.1        https://github.com/GNOME/libxml2/tags
@@ -111,7 +111,7 @@ build_packages 8.19.0 /home/lixq/toolchains/curl brotli c-ares gsasl keyutils kr
 # zstd     1.5.7         https://github.com/facebook/zstd/releases/
 # openssl  3.6.1         https://github.com/openssl/openssl/releases/
 # Python   3.14.3        https://www.python.org/ftp/python/
-build_packages 22.1.0 /home/lixq/toolchains/llvm bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
+build_packages 22.1.1 /home/lixq/toolchains/llvm bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
 
 # zsh  5.9  https://www.zsh.org/
 build_packages 5.9 /home/lixq/toolchains/zsh zsh
@@ -133,12 +133,12 @@ build_packages 1.6.1 /home/lixq/toolchains/tcpflow tcpflow
 # glibc    2.43    https://mirrors.ustc.edu.cn/gnu/glibc/
 build_packages 2.53.0 /home/lixq/toolchains/git brotli expat libpsl zlib zstd openssl curl glibc git
 
-# node   25.8.0  https://nodejs.org/dist/
+# node   25.8.1  https://nodejs.org/dist/
 # glibc  2.43    https://mirrors.tuna.tsinghua.edu.cn/gnu/glibc/
-build_packages 25.8.0 /home/lixq/toolchains/node glibc node
+build_packages 25.8.1 /home/lixq/toolchains/node glibc node
 
-# bpftrace  0.24.2  https://github.com/bpftrace/bpftrace/releases/
-build_packages 0.24.2 /home/lixq/toolchains/bpftrace bpftrace
+# bpftrace  0.25.0  https://github.com/bpftrace/bpftrace/releases/
+build_packages 0.25.0 /home/lixq/toolchains/bpftrace bpftrace
 
 # bpfsnoop  0.5.4  https://github.com/bpfsnoop/bpfsnoop/tags
 build_packages  0.5.4  /home/lixq/toolchains/bpfsnoop bpfsnoop
@@ -175,7 +175,7 @@ build_packages 0.36.1 /home/lixq/toolchains/bcc bison brotli bzip2 flex icu4c js
 # elfutils   0.194   https://sourceware.org/elfutils/ftp/
 # gcc        15.2.0  https://mirrors.tuna.tsinghua.edu.cn/gnu/gcc/
 # ncurses    6.6     https://invisible-island.net/ncurses/
-# patchelf   0.18.0  https://github.com/NixOS/patchelf/releases/
+# patchelf   0.15.5  https://github.com/NixOS/patchelf/releases/
 # readline   8.3     https://mirrors.tuna.tsinghua.edu.cn/gnu/readline/
 # xz         5.8.2   https://tukaani.org/xz/
 # zlib       1.3.2   https://github.com/madler/zlib/releases/
@@ -195,7 +195,7 @@ build_packages 4.2.0 /home/lixq/toolchains/wrk wrk
 # miniforge   26.1.0   https://mirrors.tuna.tsinghua.edu.cn/github-release/conda-forge/miniforge/
 # websocat    1.14.1   https://github.com/vi/websocat/releases/
 # Bear        4.0.3    https://github.com/rizsotto/Bear/
-# rg          5.1.0    https://github.com/BurntSushi/ripgrep/releases/
+# rg          15.1.0   https://github.com/BurntSushi/ripgrep/releases/
 # rr-debuger  5.9.0    https://github.com/rr-debugger/rr/releases/
 
 tae=$(date +%s)
--- buildsh/build_patchelf.sh
+++ buildsh/build_patchelf.sh
@@ -2,7 +2,7 @@
 
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=0.18.0
+ver=0.15.5
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver-x86_64.tar.gz
 
