16d87159e3434b1fb4ea75eaed7b36e20ad64ebb
lixq <lixq@lixq.wangsu.com>
2026-04-10 10:19:23 +0800

    llvm

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -101,20 +101,20 @@ build_packages 2.43 /opt/glibc pcre2 audit-userspace libcap glibc
 # gsasl         2.2.2   https://mirrors.tuna.tsinghua.edu.cn/gnu/gsasl/
 build_packages 8.19.0 /home/lixq/toolchains/curl brotli c-ares gsasl keyutils krb5 libidn2 libpsl libunistring zlib zstd openssl nghttp3 ngtcp2 nghttp2 libssh2 openldap curl
 
-# llvm     22.1.2        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
+# llvm     22.1.3        https://mirrors.tuna.tsinghua.edu.cn/github-release/llvm/llvm-project/
 # bison    3.8.2         https://mirrors.tuna.tsinghua.edu.cn/gnu/bison/
 # libedit  20251016-3.1  https://thrysoee.dk/editline/
 # libxml2  2.15.2        https://github.com/GNOME/libxml2/tags
 # lua      5.5.0         https://www.lua.org/ftp/
 # ncurses  6.6           https://invisible-island.net/ncurses/
 # swig     4.4.1         https://github.com/swig/swig/tags/
-# xz       5.8.2         https://tukaani.org/xz/
+# xz       5.8.3         https://tukaani.org/xz/
 # zlib     1.3.2         https://github.com/madler/zlib/releases/
 # zstd     1.5.7         https://github.com/facebook/zstd/releases/
-# openssl  3.6.1         https://github.com/openssl/openssl/releases/
-# Python   3.14.3        https://www.python.org/ftp/python/
-build_packages 22.1.2 /home/lixq/toolchains/llvm bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
-build_packages 22.1.2 /home/watch/toolchains/lldb bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
+# openssl  3.6.2         https://github.com/openssl/openssl/releases/
+# Python   3.14.4        https://www.python.org/ftp/python/
+build_packages 22.1.3 /home/lixq/toolchains/llvm bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
+build_packages 22.1.3 /home/watch/toolchains/lldb bison libedit libxml2 lua ncurses swig xz zlib zstd openssl Python llvm
 
 # zsh  5.9  https://www.zsh.org/
 build_packages 5.9 /home/lixq/toolchains/zsh zsh
