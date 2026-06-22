97fa7fe7d5688cbfb7d3f0d9f19479c6d2df757b
lixq <lixq@lixq.wangsu.com>
2026-06-22 10:32:44 +0800

    cmake

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -27,8 +27,8 @@ function build_packages() {
 tab=$(date +%s)
 date "+%Y-%m-%d %H:%M:%S begin" | tee /tmp/build_all.log
 
-# cmake  4.3.3  https://cmake.org/download/
-build_packages 4.3.3 /home/lixq/toolchains/cmake cmake
+# cmake  4.3.4  https://cmake.org/download/
+build_packages 4.3.4 /home/lixq/toolchains/cmake cmake
 
 # Shellcheck  0.11.0  https://github.com/koalaman/shellcheck/releases
 build_packages 0.11.0 /home/lixq/toolchains/shellcheck shellcheck
