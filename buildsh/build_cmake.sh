414f530e45e9b5f4f73a414e8f5ac2a790d8d99a
lixq <lixq@lixq.wangsu.com>
2026-06-22 10:30:12 +0800

    cmake

--- buildsh/build_cmake.sh
+++ buildsh/build_cmake.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=4.3.3
+ver=4.3.4
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver-linux-x86_64.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-[[ -d $DESTDIR/usr ]] || mkdir -p "$DESTDIR/usr"
+mkdir -p "$DESTDIR/usr"
 cd "$DESTDIR/usr" || exit 1
 tar -xf "$srcpath" --strip-components=1 || exit 1
