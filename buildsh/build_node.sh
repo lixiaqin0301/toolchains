106d3614351b77bc96fff1e252e706afd261b9c4
lixq <lixq@lixq.wangsu.com>
2026-06-22 10:41:28 +0800

    node

--- buildsh/build_node.sh
+++ buildsh/build_node.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=v26.3.0
+ver=v26.3.1
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="$DESTDIR/usr/bin:/home/lixq/toolchains/Miniforge3/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export CPPFLAGS="-I$DESTDIR/include --sysroot=$DESTDIR"
 export LDFLAGS=" -L$DESTDIR/lib64 -L$DESTDIR/usr/lib64 -Wl,-rpath-link,$DESTDIR/lib64:$DESTDIR/usr/lib64 --sysroot=$DESTDIR -Wl,-rpath,$DESTDIR/lib64:$DESTDIR/usr/lib64 -Wl,--dynamic-linker=$DESTDIR/lib64/ld-linux-x86-64.so.2"
 export LIBRARY_PATH="$DESTDIR/usr/lib64:$DESTDIR/lib64"
 
-[[ -d $DESTDIR/usr/lib64 ]] || mkdir -p "$DESTDIR/usr/lib64"
-cd "$DESTDIR/usr/lib64" || exit 1
+mkdir -p "$DESTDIR/usr/lib64"
+cd "$DESTDIR/usr/lib64"
 for p in /home/lixq/toolchains/gcc/usr/lib64/libgcc* /home/lixq/toolchains/gcc/usr/lib64/libstdc++.s*[0-9o]; do
     [[ -f $(basename "$p") ]] && continue
     if [[ -L $p ]]; then
@@ -24,14 +23,14 @@ for p in /home/lixq/toolchains/gcc/usr/lib64/libgcc* /home/lixq/toolchains/gcc/u
         cp "$p" .
     fi
 done
-[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
-cd /home/lixq/src || exit 1
+
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
-cd "$name-$ver" || exit 1
-./configure "--prefix=$DESTDIR/usr" || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+tar -xf "$srcpath"
+cd "/home/lixq/src/$name-$ver"
+./configure "--prefix=$DESTDIR/usr"
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install
 
 while IFS= read -r f; do
     $f --help 2>&1 | grep -q GLIBC || continue
