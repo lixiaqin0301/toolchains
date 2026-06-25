cc80b2dba853cd88eb45e7bae63cceef146df46c
lixq <lixq@lixq.wangsu.com>
2026-06-25 20:03:19 +0800

    libpsl

--- buildsh/build_libpsl.sh
+++ buildsh/build_libpsl.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=0.21.5
+ver=0.22.0
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
-cd /home/lixq/src || exit 1
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
+tar -xf "$srcpath"
 mkdir "$name-$ver/build"
-cd "$name-$ver/build" || exit 1
-../configure "--prefix=$DESTDIR/usr" || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+cd "/home/lixq/src/$name-$ver/build"
+../configure --prefix=/usr
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install DESTDIR="$DESTDIR"
