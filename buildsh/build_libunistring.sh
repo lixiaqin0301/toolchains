3a3aead69a9f0663e2612456186f745cd3e20b24
lixq <lixq@lixq.wangsu.com>
2026-06-25 20:11:28 +0800

    libunistring

--- buildsh/build_libunistring.sh
+++ buildsh/build_libunistring.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
 ver=1.4.2
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
-cd /home/lixq/src || exit 1
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
-cd "$name-$ver" || exit 1
-./configure "--prefix=$DESTDIR/usr" || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+tar -xf "$srcpath"
+cd "/home/lixq/src/$name-$ver"
+./configure --prefix=/usr
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install DESTDIR="$DESTDIR"
