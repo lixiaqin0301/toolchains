60b1fdd34da911de8a2b0f31cb03c8796d1de247
lixq <lixq@lixq.wangsu.com>
2026-06-25 20:36:29 +0800

    ngtcp2

--- buildsh/build_ngtcp2.sh
+++ buildsh/build_ngtcp2.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=1.22.1
+ver=1.23.0
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export PKG_CONFIG_PATH="$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_PATH="$DESTDIR/lib64/pkgconfig:$DESTDIR/usr/lib64/pkgconfig:$DESTDIR/lib/pkgconfig:$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$DESTDIR"
 export CPATH="$DESTDIR/usr/include"
 export LIBRARY_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib"
 
-cd /home/lixq/src || exit 1
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
-cd "$name-$ver" || exit 1
-autoreconf -fi || exit 1
-./configure "--prefix=$DESTDIR/usr" --with-openssl || exit 1
-
-[[ -f tests/munit/.deps/examplestest-munit.Po ]] || cp "examples/\$(top_srcdir)/tests/munit/.deps/examplestest-munit.Po" tests/munit/.deps/examplestest-munit.Po
-
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+tar -xf "$srcpath"
+cd "$name-$ver"
+autoreconf -fi
+./configure --prefix=/usr --with-openssl
+#[[ -f tests/munit/.deps/examplestest-munit.Po ]] || cp "examples/\$(top_srcdir)/tests/munit/.deps/examplestest-munit.Po" tests/munit/.deps/examplestest-munit.Po
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install DESTDIR="$DESTDIR"
