91c3078d7ba574714517c90794e4e1260b0c7404
lixq <lixq@lixq.wangsu.com>
2026-06-25 20:28:59 +0800

    nghttp3

--- buildsh/build_nghttp3.sh
+++ buildsh/build_nghttp3.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=1.15.0
+ver=1.16.0
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
-[[ -f /home/lixq/src/sfparse.tar.gz ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export PKG_CONFIG_PATH="$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_PATH="$DESTDIR/lib64/pkgconfig:$DESTDIR/usr/lib64/pkgconfig:$DESTDIR/lib/pkgconfig:$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$DESTDIR"
 export CPATH="$DESTDIR/usr/include"
 export LIBRARY_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib"
 
-cd /home/lixq/src || exit 1
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
-cd "/home/lixq/src/$name-$ver/lib" || exit 1
-tar -xf /home/lixq/src/sfparse.tar.gz || exit 1
-cd "/home/lixq/src/$name-$ver" || exit 1
-autoreconf -fi || exit 1
-./configure "--prefix=$DESTDIR/usr" || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+tar -xf "$srcpath"
+cd "/home/lixq/src/$name-$ver/lib"
+git clone https://github.com/ngtcp2/sfparse
+cd "/home/lixq/src/$name-$ver"
+autoreconf -fi
+./configure --prefix=/usr
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install DESTDIR="$DESTDIR"
