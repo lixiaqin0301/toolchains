e614dc864911c7350f3a42f78033ba38d9df3d76
lixq <lixq@lixq.wangsu.com>
2026-06-26 09:48:46 +0800

    libssh2

--- buildsh/build_libssh2.sh
+++ buildsh/build_libssh2.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-
+set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
 ver=1.11.1
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
-
-[[ -n $DESTDIR ]] || exit 1
-[[ -f $srcpath ]] || exit 1
+[[ -n $DESTDIR ]]
+[[ -f $srcpath ]]
 
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export PKG_CONFIG_PATH="$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_PATH="$DESTDIR/lib64/pkgconfig:$DESTDIR/usr/lib64/pkgconfig:$DESTDIR/lib/pkgconfig:$DESTDIR/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$DESTDIR"
 export CPATH="$DESTDIR/usr/include"
 export LIBRARY_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib"
 
-[[ -d /home/lixq/src ]] || mkdir /home/lixq/src
-cd /home/lixq/src || exit 1
+cd /home/lixq/src
 rm -rf "$name-$ver"
-tar -xf "$srcpath" || exit 1
-cd "$name-$ver" || exit 1
-./configure "--prefix=$DESTDIR/usr" || exit 1
-make -s "-j$(nproc)" || exit 1
-make -s "-j$(nproc)" install || exit 1
+tar -xf "$srcpath"
+cd "$name-$ver"
+./configure --prefix=/usr
+make -s "-j$(nproc)"
+make -s "-j$(nproc)" install DESTDIR="$DESTDIR"
