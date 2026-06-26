42280d7a9049afb871f1929bf50d6f9224e995d1
lixq <lixq@lixq.wangsu.com>
2026-06-26 16:28:57 +0800

    keyutils

--- buildsh/build_keyutils.sh
+++ buildsh/build_keyutils.sh
@@ -16,3 +16,14 @@ tar -xf "$srcpath"
 cd "/home/lixq/src/$name-$ver"
 make -s "-j$(nproc)"
 make -s "-j$(nproc)" install DESTDIR="$DESTDIR" PREFIX=/usr
+
+cd "$DESTDIR"/lib64
+rm libkeyutils.so
+ln -s libkeyutils.so.1 libkeyutils.so
+
+for d in lib lib64; do
+    mkdir -p "$DESTDIR"/usr/$d
+    cd "$DESTDIR"/usr/$d
+    ln -s ../../lib64/libkeyutils.so.1.* libkeyutils.so.1
+    ln -s libkeyutils.so.1 libkeyutils.so
+done
