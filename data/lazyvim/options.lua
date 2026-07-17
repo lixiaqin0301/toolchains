f134de6784b771d153ce4a6788a7985e4950ecec
lixq <lixq@lixq.wangsu.com>
2026-07-17 15:38:48 +0800

    lazyvim

---
+++ data/lazyvim/options.lua
@@ -0,0 +1,21 @@
+vim.opt.relativenumber = false
+vim.opt.conceallevel = 0
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = function()
+			return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+		end,
+		["*"] = function()
+			return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+		end,
+	},
+}
