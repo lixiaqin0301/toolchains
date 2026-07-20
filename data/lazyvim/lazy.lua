a2e62180ff259eec6f998f0013df6e9494510cb9
lixq <lixq@lixq.wangsu.com>
2026-07-20 15:28:23 +0800

    lazyvim python

--- data/lazyvim/lazy.lua
+++ data/lazyvim/lazy.lua
@@ -1,4 +1,5 @@
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
