7aa40512a8c8cc0fa6a178af1f4b2abf71cd472e
lixq <lixq@lixq.wangsu.com>
2026-07-21 12:15:44 +0800

    LazyVim clang-format

---
+++ data/lazyvim/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      c = { "clang_format" },
+      cpp = { "clang_format" },
+      objc = { "clang_format" },
+      objcpp = { "clang_format" },
+      cuda = { "clang_format" },
+      proto = { "clang_format" },
+    },
+  },
+}
