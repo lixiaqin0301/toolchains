afde4dccea6aada3dbde73ddb1354d42ed1d72aa
lixq <lixq@lixq.wangsu.com>
2026-07-21 13:25:00 +0800

    LazyVim util.dot

--- data/lazyvim/lazy.lua
+++ data/lazyvim/lazy.lua
@@ -4,3 +4,4 @@
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.util.dot" },
