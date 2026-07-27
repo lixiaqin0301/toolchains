ff85118be84067303a4dec24ef3d65ddeaf7776a
lixq <lixq@lixq.wangsu.com>
2026-07-27 12:36:24 +0800

    glibc

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -79,7 +79,6 @@ build_packages 4.4.1 /home/lixq/toolchains/make make
 # pcre2            10.47    https://github.com/PCRE2Project/pcre2/releases/
 # audit-userspace  4.2      https://github.com/linux-audit/audit-userspace/releases/
 # libcap           2.78     https://git.kernel.org/pub/scm/libs/libcap/libcap.git/
-build_packages 2.44 /home/lixq/toolchains/glibc pcre2 audit-userspace libcap glibc
 build_packages 2.44 /opt/glibc pcre2 audit-userspace libcap glibc
 
 # curl          8.21.0  https://github.com/curl/curl/releases/
