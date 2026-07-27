6721d1aeed502a5b10ccce6cf59880292f805f07
lixq <lixq@lixq.wangsu.com>
2026-07-27 11:29:53 +0800

    audit-userspace

--- buildsh/build_audit-userspace.sh
+++ buildsh/build_audit-userspace.sh
@@ -14,8 +14,13 @@ cd /home/lixq/src
 rm -rf "$name-$ver"
 tar -xf "$srcpath"
 cd "/home/lixq/src/$name-$ver"
+sed -i '/#include <linux\/netlink.h>/a #ifndef SOL_NETLINK\
+#define SOL_NETLINK 270\
+#endif' src/auditd.c
 autoreconf -fi
 ./configure "--prefix=$DESTDIR/usr" --disable-zos-remote --with-python3=no
 cp -ar "src/test/\${top_srcdir}/src/.deps" src
+cp -ar "audisp/test/\${top_srcdir}/audisp/.deps" audisp
+cp -ar "tools/aulast/test/\${top_srcdir}/tools/aulast/.deps" tools/aulast
 make -s "-j$(nproc)"
 make -s "-j$(nproc)" install
