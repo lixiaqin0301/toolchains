259fe5e384379a0ae02b7d066b3dd7b2446413bd
lixq <lixq@lixq.wangsu.com>
2026-09-03 10:34:51 +0800

    c-ares

--- buildsh/build_c-ares.sh
+++ buildsh/build_c-ares.sh
@@ -2,13 +2,18 @@
 set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=1.34.6
+ver=1.34.8
 DESTDIR=$1
 srcpath=/home/lixq/src/$name-$ver.tar.gz
 [[ -n $DESTDIR ]]
 [[ -f $srcpath ]]
 
-export PATH="/home/lixq/toolchains/cmake/usr/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export MANPATH=
+export PCP_DIR=
+export LD_LIBRARY_PATH=
+export PKG_CONFIG_PATH=
+export INFOPATH=
+export PATH="/home/lixq/toolchains/cmake/usr/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin"
 
 cd /home/lixq/src
 rm -rf "$name-$ver"
@@ -16,5 +21,5 @@ tar -xf "$srcpath"
 mkdir "$name-$ver/out"
 cd "/home/lixq/src/$name-$ver/out"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$DESTDIR"/usr ..
-cmake --build . --config Release
+cmake --build . --config Release -j"$(nproc)"
 cmake --install . --config Release
