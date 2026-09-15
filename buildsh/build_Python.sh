1f43f4c2113444f2b5ef3c789729df77c5b24c30
lixq <lixq@lixq.wangsu.com>
2026-09-15 19:27:08 +0800

    Python

--- buildsh/build_Python.sh
+++ buildsh/build_Python.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=3.14.7
-DESTDIR=$1
-srcpath=/home/lixq/src/${name}-${ver}.tar.xz
+ver=$1
+DESTDIR=$2
+srcpath=/share-rd/cdn_prd_cache/lixq/src/${name}-${ver}.tar.xz
 [[ -n $DESTDIR ]]
 [[ -f $srcpath ]]
 
