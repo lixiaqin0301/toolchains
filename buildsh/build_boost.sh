9f597049eeab459d1c2a642959861133fc5c6471
lixq <lixq@lixq.wangsu.com>
2026-09-16 19:40:16 +0800

    boost

--- buildsh/build_boost.sh
+++ buildsh/build_boost.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=1_92_0
-DESTDIR=$1
-srcpath=/home/lixq/src/${name}_$ver.tar.gz
+ver=$1
+DESTDIR=$2
+srcpath=/share-rd/cdn_prd_cache/lixq/src/${name}_$ver.tar.gz
 [[ -n $DESTDIR ]]
 [[ -f $srcpath ]]
 
