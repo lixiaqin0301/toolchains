48e98015795dadd339c544e1581ab010eacc7ade
lixq <lixq@lixq.wangsu.com>
2026-09-17 14:57:01 +0800

    git tcpflow bpfsnoop

--- buildsh/build_all.sh
+++ buildsh/build_all.sh
@@ -39,23 +39,6 @@ function build_packages() {
 tab=$(date +%s)
 date "+%Y-%m-%d %H:%M:%S begin" | tee /tmp/build_all.log
 
-# tcpflow  1.6.1  https://github.com/simsong/tcpflow/releases/
-build_packages 1.6.1 /home/lixq/toolchains/tcpflow tcpflow
-
-# git      2.55.0  https://github.com/git/git/tags/
-# brotli   1.2.0   https://github.com/google/brotli/releases/
-# expat    2.8.2   https://github.com/libexpat/libexpat/releases/
-# libpsl   0.22.0  https://github.com/rockdaboot/libpsl/releases/
-# zlib     1.3.2   https://github.com/madler/zlib/releases/
-# zstd     1.5.7   https://github.com/facebook/zstd/releases/
-# openssl  4.0.1   https://github.com/openssl/openssl/releases/
-# curl     8.21.0  https://github.com/curl/curl/releases/
-# glibc    2.43    https://mirrors.ustc.edu.cn/gnu/glibc/
-build_packages 2.55.0 /home/lixq/toolchains/git brotli expat libpsl zlib zstd openssl curl glibc git
-
-# bpfsnoop  0.5.5  https://github.com/bpfsnoop/bpfsnoop/tags
-build_packages  0.5.5  /home/lixq/toolchains/bpfsnoop bpfsnoop
-
 # bcc       0.37.0         https://github.com/iovisor/bcc/releases/
 # bison     3.8.2          https://mirrors.tuna.tsinghua.edu.cn/gnu/bison/
 # brotli    1.2.0          https://github.com/google/brotli/releases/
