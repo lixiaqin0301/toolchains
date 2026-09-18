3233c66c62ab4c43b8664f44909f3e88f5586403
lixq <lixq@lixq.wangsu.com>
2026-09-18 11:00:15 +0800

    elfutils

--- buildsh/build_elfutils.sh
+++ buildsh/build_elfutils.sh
@@ -2,12 +2,17 @@
 set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=0.195
-DESTDIR=$1
-srcpath=/home/lixq/src/$name-$ver.tar.bz2
+ver=$1
+DESTDIR=$2
+srcpath=/share-rd/cdn_prd_cache/lixq/src/$name-$ver.tar.bz2
 [[ -n $DESTDIR ]]
 [[ -f $srcpath ]]
 
+export MANPATH=
+export PCP_DIR=
+export LD_LIBRARY_PATH=
+export PKG_CONFIG_PATH=
+export INFOPATH=
 export PATH="/home/lixq/toolchains/gcc/usr/bin:/home/lixq/toolchains/make/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export PKG_CONFIG_PATH="$DESTDIR/lib64/pkgconfig:$DESTDIR/usr/lib64/pkgconfig:$DESTDIR/usr/lib/pkgconfig"
 export LD_RUN_PATH="$DESTDIR/lib64:$DESTDIR/usr/lib64:$DESTDIR/lib:$DESTDIR/usr/lib"
