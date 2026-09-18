caf68419c147120f230282e17c5c11d4b6b0aec5
lixq <lixq@lixq.wangsu.com>
2026-09-18 12:12:55 +0800

    libpsl

--- buildsh/build_libpsl.sh
+++ buildsh/build_libpsl.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 name=$(basename "${BASH_SOURCE[0]}" .sh)
 name=${name#build_}
-ver=0.23.3
-DESTDIR=$1
-srcpath=/home/lixq/src/$name-$ver.tar.gz
+ver=$1
+DESTDIR=$2
+srcpath=/share-rd/cdn_prd_cache/lixq/src/$name-$ver.tar.gz
 [[ -n $DESTDIR ]]
 [[ -f $srcpath ]]
 
@@ -14,6 +14,17 @@ export LD_LIBRARY_PATH=
 export PKG_CONFIG_PATH=
 export INFOPATH=
 export PATH="/home/lixq/toolchains/make/usr/bin:/home/lixq/toolchains/gcc/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin"
+
+mkdir -p "$DESTDIR/usr/lib64"
+cd "$DESTDIR/usr/lib64"
+for p in /home/lixq/toolchains/gcc/usr/lib64/libgcc* /home/lixq/toolchains/gcc/usr/lib64/libstdc++.s*[0-9o]; do
+    [[ -f $(basename "$p") ]] && continue
+    if [[ -L $p ]]; then
+        ln -sf "$(readlink "$p")" "$(basename "$p")"
+    else
+        cp "$p" .
+    fi
+done
 
 cd /home/lixq/src
 rm -rf "$name-$ver"
